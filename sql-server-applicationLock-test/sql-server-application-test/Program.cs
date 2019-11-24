412e93d33c90822dc8aefdda5aab4265fda3c65e
ManagerSi <smallvv@126.com>
2019-11-24 21:30:15 +0800

    新建测试项目，数据库应用程序锁示例

---
+++ sql-server-applicationLock-test/sql-server-application-test/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sql_server_application_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================================Hello World!================================");
+
+            //ApplicationToRun("app1");
+
+            //同步，没有问题
+            MockMutiApplicationToRun();
+
+            //异步-->Parallel类的For方法并没有等待延迟，而是直接完成. parallel类只等待它创建的任务，而不等待其它后台活动
+            //MockMutiApplicationToRunAsycn();
+
+            Console.WriteLine("===================================Down!==================================");
+            Console.ReadKey();
+        }
+
+        #region 同步方法
+        /// <summary>
+        /// https://www.cnblogs.com/afei-24/p/6904179.html
+        /// 使用Parallel.ForEach 开启多个app
+        /// </summary>
+        static void MockMutiApplicationToRun()
+        {
+            var list = new List<int> {1, 2, 3, 4, 5};
+            ParallelLoopResult plr = Parallel.ForEach(list , s =>
+            {
+                ApplicationToRun("appServer" + s);
+            });
+
+            if (plr.IsCompleted)
+                Console.WriteLine("all appServer completed!");
+        }
+
+        /// <summary>
+        /// 开启一个app server
+        /// </summary>
+        /// <param name="appServerName">设置appServerName</param>
+        static void ApplicationToRun(string appServerName)
+        {
+            string connStr = "Data Source=localhost;Integrated Security=SSPI;user id=jerry;password=jerry";
+            using (SqlLock sqlLock = new SqlLock(connStr, "App_lockKey", 3000))
+            {
+                if (sqlLock.LockCreated)
+                {
+                    Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::get lock.");
+                    
+                    ProcessDataForLongTime(appServerName);
+                }
+                else
+                {
+                    Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::can't get lock.");
+                }
+            }
+            Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::finished.");
+        }
+
+        /// <summary>
+        /// 处理数据的方法
+        /// </summary>
+        /// <param name="appServerName">设置appServerName</param>
+        static void ProcessDataForLongTime(string appServerName)
+        {
+            Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::ProcessData.");
+            Task.Delay(TimeSpan.FromSeconds(10)).GetAwaiter().GetResult();
+            //Thread.Sleep(TimeSpan.FromSeconds(15));
+        }
+        #endregion 同步方法
+
+
+        #region 异步方法
+        /// <summary>
+        /// 使用Parallel.ForEach 开启多个app
+        /// </summary>
+        static void MockMutiApplicationToRunAsycn()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            ParallelLoopResult plr = Parallel.ForEach(list, async s =>
+            {
+               await ApplicationToRunAsync("appServer" + s);
+            });
+
+            if (plr.IsCompleted)
+                Console.WriteLine("all appServer completed!");
+        }
+
+        /// <summary>
+        /// 开启一个app server
+        /// </summary>
+        /// <param name="appServerName">设置appServerName</param>
+        static async Task ApplicationToRunAsync(string appServerName)
+        {
+            string connStr = "Data Source=localhost;Integrated Security=SSPI;user id=jerry;password=jerry";
+            using (SqlLock sqlLock = new SqlLock(connStr, "App_lockKey", 3000))
+            {
+                if (sqlLock.LockCreated)
+                {
+                    Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::get lock.");
+
+                    await ProcessDataForLongTimeAsync(appServerName);
+                }
+                else
+                {
+                    Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::can't get lock.");
+                }
+            }
+            Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::finished.");
+        }
+
+        /// <summary>
+        /// 异步处理数据的方法
+        /// 线程不是由parallel生成，所以parallel不会等待该线程
+        /// </summary>
+        /// <param name="appServerName">设置appServerName</param>
+        static async Task ProcessDataForLongTimeAsync(string appServerName)
+        {
+            Console.WriteLine($"CurrentThread:{Thread.CurrentThread.ManagedThreadId}  {appServerName}::ProcessData.");
+            await Task.Delay(TimeSpan.FromSeconds(10));
+        }
+        #endregion 异步方法
+
+
+    }
+}
---
+++ sql-server-applicationLock-test/sql-server-application-test/SqlLock.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace sql_server_application_test
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-getapplock-transact-sql?view=sql-server-ver15
+    /// 使用AppKey 获取数据库锁，多个应用程序使用相同AppKey同时请求时，只会有一个获得锁
+    /// </summary>
+    public class SqlLock:IDisposable
+    {
+        private readonly string _dbPrincipal = "public";
+        private readonly string _appKey = "MyAppLock";
+        private readonly string _lockMode = "Exclusive";
+        /// <summary>
+        /// 获取锁的超时时长，如1000ms内得不到锁就抛异常
+        /// </summary>
+        private readonly int _lockTimeout = 1000;
+        private readonly string _lockOwner = "Transaction";
+
+        private SqlConnection _connection;
+        private SqlTransaction _transaction;
+
+        public bool LockCreated;
+
+        public SqlLock(string connectionStr, string appKey, int tLockTimeout=1000)
+        {
+            _appKey = appKey;
+            _lockTimeout = tLockTimeout;
+            _connection = new SqlConnection(connectionStr);
+            _connection.Open();
+            CreateLock();
+        }
+        public void Dispose()
+        {
+            //Release the Application Lock if it was created
+            if (LockCreated)
+            {
+                ReleaseLock();
+            }
+
+            _connection.Close();
+            _connection.Dispose();
+        }
+        private void CreateLock()
+        {
+            _transaction = _connection.BeginTransaction();
+
+            using (SqlCommand createCmd = _connection.CreateCommand())
+            {
+                createCmd.Transaction = _transaction;
+                createCmd.CommandType = System.Data.CommandType.Text;
+
+                createCmd.CommandText = $@"DECLARE @result int;  
+                                            EXEC @result=sp_getapplock  
+                                                @DbPrincipal='{_dbPrincipal}',  
+                                                @Resource='{_appKey}',  
+                                                @LockMode='{_lockMode}',  
+	                                            @LockTimeout={_lockTimeout.ToString()},
+                                                @LockOwner='{_lockOwner}';  
+                                            IF @result NOT IN (0, 1)
+                                            BEGIN
+                                                RAISERROR ( 'Unable to acquire Lock', 16, 1 )
+                                            END";
+
+                try
+                {
+                    createCmd.ExecuteNonQuery();
+                    LockCreated = true;
+                }
+                catch (Exception ex)
+                {
+                    _transaction.Rollback();
+                    //throw new Exception(string.Format("Unable to get SQL Application Lock on '{0}'", AppKey), ex);
+                }
+            }
+        }
+        private void ReleaseLock()
+        {
+            using (SqlCommand releaseCmd = _connection.CreateCommand())
+            {
+                releaseCmd.Transaction = _transaction;
+                releaseCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                releaseCmd.CommandText = "sp_releaseapplock";
+
+                releaseCmd.Parameters.AddWithValue("@Resource", _appKey);
+                releaseCmd.Parameters.AddWithValue("@LockOwner", _lockOwner);
+                releaseCmd.Parameters.AddWithValue("@DbPrincipal", _dbPrincipal);
+
+                try
+                {
+                    releaseCmd.ExecuteNonQuery();
+                }
+                catch { }
+            }
+
+            _transaction.Commit();
+        }
+    }
+}
