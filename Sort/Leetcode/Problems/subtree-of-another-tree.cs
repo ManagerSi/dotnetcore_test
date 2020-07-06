6a4e8079224b061324a4e46a47c5e5564c203e1b
jerry.si <403563310@qq.com>
2020-07-06 22:37:41 +0800

    572. 另一个树的子树

---
+++ Sort/Leetcode/Problems/subtree-of-another-tree.cs
@@ -0,0 +1,35 @@
+﻿using Leetcode.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 572. 另一个树的子树
+    /// https://leetcode-cn.com/problems/subtree-of-another-tree/
+    /// </summary>
+    public class subtree_of_another_tree
+    {
+        public bool IsSubtree(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null) return true;
+            if (s != null && t == null) return true;
+            if (s == null && t != null) return false;
+
+            return CheckNode(s, t) || IsSubtree(s.left, t) || IsSubtree(s.right, t);
+        }
+        private  bool CheckNode(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null)
+                return true;
+            if (s == null || t == null)
+                return false;
+            if (s.val == t.val && CheckNode(s.left, t.left) && CheckNode(s.right, t.right))
+                return true;
+
+            return false;
+        }
+    }
+
+}
--- Sort/LeetcodeTests/UnitTest1.cs
+++ Sort/LeetcodeTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Leetcode.Model;
+using Leetcode.Problems;
 using NUnit.Framework;
 
 namespace LeetcodeTests
@@ -13,6 +15,37 @@ namespace LeetcodeTests
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public void subtree_of_another_tree_test()
+        {
+            var t = new TreeNode(4,new TreeNode(1),new TreeNode(2));
+            var s = new TreeNode(3, t, new TreeNode(5));
+
+            var res = new subtree_of_another_tree().IsSubtree(s, t);
+
+            Assert.IsTrue(res);
+        }
+        [Test]
+        public void subtree_of_another_tree_test2()
+        {
+            var t = new TreeNode(1);
+            var s = new TreeNode(0);
+
+            var res = new subtree_of_another_tree().IsSubtree(s, t);
+
+            Assert.False(res);
+        }
+        [Test]
+        public void subtree_of_another_tree_test3()
+        {
+            var t = new TreeNode(1);
+            var s = new TreeNode(1, new TreeNode(1));
+
+            var res = new subtree_of_another_tree().IsSubtree(s, t);
+
+            Assert.True(res);
         }
     }
 }
