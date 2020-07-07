59e1a3c4716b1a02637ed8d392d15bc871cf732e
jerry.si <403563310@qq.com>
2020-07-07 20:50:05 +0800

    二叉树 112. 路径总和

---
+++ Sort/Leetcode/Problems/HasPathSum.cs
@@ -0,0 +1,37 @@
+﻿using Leetcode.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 112. 路径总和
+    /// https://leetcode-cn.com/problems/path-sum/
+    /// </summary>
+    public class Has_Path_Sum
+    {
+        /// <summary>
+        /// 思路：深度优先遍历
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="sum"></param>
+        /// <returns></returns>
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+                return false;
+
+            if (root.left == null && root.right == null)
+                return sum - root.val == 0;
+
+            if (HasPathSum(root.left, sum - root.val))
+                return true;
+
+            if (HasPathSum(root.right, sum - root.val))
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/Has_Path_Sum_Test.cs
@@ -0,0 +1,34 @@
+﻿using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    public class Has_Path_Sum_Test
+    {
+        [Test]
+        public void test1()
+        {
+            var tree = new TreeNode(5, new TreeNode(4, new TreeNode(11)), new TreeNode(8, new TreeNode(13), new TreeNode(4)));
+            var target = 17;
+
+            var res = new Has_Path_Sum().HasPathSum(tree, target);
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void test2()
+        {
+            TreeNode tree = null;
+            var target = 0;
+
+            var res = new Has_Path_Sum().HasPathSum(tree, target);
+
+            Assert.IsFalse(res);
+        }
+    }
+}
