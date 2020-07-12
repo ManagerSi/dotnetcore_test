dbc2fe7cc3c8655b2dc9732459b3dd4c426c6f59
ManagerSi <smallvv@126.com>
2020-07-12 11:22:11 +0800

    回文数

---
+++ Sort/Leetcode/Problems/palindrome-number.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 9. 回文数
+    /// https://leetcode-cn.com/problems/palindrome-number/
+    /// </summary>
+    public class palindrome_number
+    {
+        /// <summary>
+        /// 转换为字符串
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public bool isPalindrome(int x)
+        {
+            if (x == 0) return true;
+
+            //负数不是 / 以0结尾的不是
+            if (x < 0 || x % 10 == 0)
+                return false;
+
+            string str = x.ToString();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 直观上来看待回文数的话，就感觉像是将数字进行对折后看能否一一对应。
+        /// 所以这个解法的操作就是 取出后半段数字进行翻转。
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public bool isPalindrome_V2(int x)
+        {
+            if (x == 0) return true;
+
+            //负数不是 / 以0结尾的不是
+            if (x < 0 || x % 10 == 0)
+                return false;
+
+
+            int xRevers = 0;
+            while (xRevers < x)
+            {
+                xRevers = xRevers* 10 + x % 10;
+                if (xRevers >=x)
+                {
+                    break;
+                }
+                x = x / 10;
+            }
+
+            return xRevers == x;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/palindrome-number-test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class palindrome_number_test
+    {
+        [Test]
+        public void test()
+        {
+            var x = 12321;
+            var res = new palindrome_number().isPalindrome(x);
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void test_0()
+        {
+            var x = 0;
+            var res = new palindrome_number().isPalindrome(x);
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void test_V2_test()
+        {
+            var x = 12321;
+            var res = new palindrome_number().isPalindrome_V2(x);
+
+            Assert.IsTrue(res);
+        }
+        [Test]
+        public void test_V2_test2()
+        {
+            var x = 1221;
+            var res = new palindrome_number().isPalindrome_V2(x);
+
+            Assert.IsTrue(res);
+        }
+    }
+}
