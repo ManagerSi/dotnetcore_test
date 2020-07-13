97bf51465ed8c74401bfbf05a7557ba85ae9bf0b
ManagerSi <smallvv@126.com>
2020-07-13 21:13:07 +0800

    104. 二叉树的最大深度

---
+++ Sort/Leetcode/Problems/maximum-depth-of-binary-tree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    public class maximum_depth_of_binary_tree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return NodeDepth(root, 0);
+        }
+        private int NodeDepth(TreeNode root, int depth)
+        {
+            if (root == null)
+                return depth;
+            depth++;
+            var leftDepth = NodeDepth(root.left, depth);
+            var rightDepth = NodeDepth(root.right, depth);
+
+            return leftDepth > rightDepth ? leftDepth : rightDepth;
+        }
+
+
+
+        //方法2，使用公共变量，减少内存
+        private int iMaxDepth=0;
+        public int MaxDepth_V2(TreeNode root)
+        {
+            NodeDepth_V2(root, 0);
+            return iMaxDepth;
+        }
+
+        private void NodeDepth_V2(TreeNode root, int depth)
+        {
+            if (root == null)
+                return;
+
+            depth++;
+
+            iMaxDepth = depth > iMaxDepth ? depth : iMaxDepth;
+
+            NodeDepth_V2(root.left, depth);
+            NodeDepth_V2(root.right, depth);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/maximum-depth-of-binary-tree-test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class maximum_depth_of_binary_tree_test
+    {
+        [Test]
+        public void test()
+        {
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var res = new maximum_depth_of_binary_tree().MaxDepth(root);
+
+            Assert.IsTrue(res == 3);
+        }
+
+        [Test]
+        public void test_V2()
+        {
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var res = new maximum_depth_of_binary_tree().MaxDepth_V2(root);
+
+            Assert.IsTrue(res == 3);
+        }
+    }
+}
