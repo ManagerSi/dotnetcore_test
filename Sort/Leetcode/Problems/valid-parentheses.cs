7c9642f36b6b5b72b354cac40ca85a0ac3378bc5
ManagerSi <smallvv@126.com>
2020-07-14 21:19:05 +0800

    20. 有效的括号

---
+++ Sort/Leetcode/Problems/valid-parentheses.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 20. 有效的括号
+    /// https://leetcode-cn.com/problems/valid-parentheses/
+    ///
+    /// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+    /// </summary>
+    public class valid_parentheses
+    {
+        private Dictionary<char, char> hesesDict = new Dictionary<char, char>()
+        {
+            { '(', ')'},
+            { '{', '}'},
+            { '[', ']'},
+        };
+
+        /// <summary>
+        /// 使用栈和字典共同作用
+        /// </summary>
+        public bool IsValid(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return true;
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var VARIABLE in s)
+            {
+                if(hesesDict.ContainsKey(VARIABLE))
+                    stack.Push(hesesDict[VARIABLE]);
+                else
+                    if (stack.Count == 0 || stack.Pop() != VARIABLE)
+                        return false;
+            }
+
+            if (stack.Count > 0)
+                return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// hard code
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+
+        public bool IsValid_V2(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return true;
+
+            Stack<char> stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (c == '(')
+                {
+                    stack.Push(')');
+                }
+                else if (c == '{')
+                {
+                    stack.Push('}');
+                }
+                else if (c == '[')
+                {
+                    stack.Push(']');
+                }
+                else if (stack.Count==0 || c != stack.Pop())
+                {
+                    return false;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/valid-parentheses-test.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class valid_parentheses_test
+    {
+        [Test]
+        public void test_success()
+        {
+            var validClass = new valid_parentheses();
+
+            string s = "()";
+            var res = validClass.IsValid(s);
+            Assert.True(res);
+
+            s = "()[]{}";
+             res = validClass.IsValid(s);
+            Assert.True(res);
+        }
+
+        [Test]
+        public void test_empty()
+        {
+            var validClass = new valid_parentheses();
+
+            var s = "";
+            var res = validClass.IsValid(s);
+            Assert.True(res);
+
+            s = null;
+            res = validClass.IsValid(s);
+            Assert.True(res);
+        }
+
+        [Test]
+        public void test_false()
+        {
+            var validClass = new valid_parentheses();
+
+            var s = "(]";
+            var res = validClass.IsValid(s);
+            Assert.False(res);
+
+            s = "(]";
+            res = validClass.IsValid(s);
+            Assert.False(res);
+
+            s = "]";
+            res = validClass.IsValid(s);
+            Assert.False(res);
+            
+        }
+
+        #region V2
+        [Test]
+        public void test_V2_success()
+        {
+            var validClass = new valid_parentheses();
+
+            string s = "()";
+            var res = validClass.IsValid_V2(s);
+            Assert.True(res);
+
+            s = "()[]{}";
+            res = validClass.IsValid_V2(s);
+            Assert.True(res);
+        }
+
+        [Test]
+        public void test_V2_empty()
+        {
+            var validClass = new valid_parentheses();
+
+            var s = "";
+            var res = validClass.IsValid_V2(s);
+            Assert.True(res);
+
+            s = null;
+            res = validClass.IsValid_V2(s);
+            Assert.True(res);
+        }
+
+        [Test]
+        public void test_V2_false()
+        {
+            var validClass = new valid_parentheses();
+
+            var s = "(]";
+            var res = validClass.IsValid_V2(s);
+            Assert.False(res);
+
+            s = "(]";
+            res = validClass.IsValid_V2(s);
+            Assert.False(res);
+
+            s = "]";
+            res = validClass.IsValid_V2(s);
+            Assert.False(res);
+
+        }
+        #endregion
+    }
+}
