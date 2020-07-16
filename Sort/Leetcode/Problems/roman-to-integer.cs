52360f9722ff3630053c1862604ad887063014fa
ManagerSi <smallvv@126.com>
2020-07-16 21:09:10 +0800

    13. 罗马数字转整数

---
+++ Sort/Leetcode/Problems/roman-to-integer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 13. 罗马数字转整数
+    /// 
+    /// https://leetcode-cn.com/problems/roman-to-integer/
+    /// </summary>
+    
+    public class roman_to_integer
+    {
+        private Dictionary<char,int> dict = new Dictionary<char, int>()
+        {
+            {'I',             1       },
+            {'V',             5       },
+            {'X',             10      },
+            {'L',             50      },
+            {'C',             100     },
+            {'D',             500     },
+            {'M',             1000    },
+            {'a',             4    },
+            {'b',             9    },
+            {'c',             40    },
+            {'d',             90    },
+            {'e',             400    },
+            {'f',             900    },
+        };
+        private Dictionary<string, string> replaceDict = new Dictionary<string, string>()
+        {
+            {"IV", "a"  },
+            {"IX", "b"  },
+            {"XL", "c"  },
+            {"XC", "d"  },
+            {"CD", "e"  },
+            {"CM", "f"  },
+        };
+        /// <summary>
+        /// 使用dict提前定义
+        /// 使用string的replace
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int RomanToInt(string s)
+        {
+            foreach (var rep in replaceDict)
+            {
+                s = s.Replace(rep.Key, rep.Value);
+            }
+
+            int res = 0;
+            foreach (var c in s)
+            {
+                res += dict.ContainsKey(c) ? dict[c] : 0;
+            }
+
+            return res;
+        }
+    }
+
+}
---
+++ Sort/LeetcodeTests/roman-to-integer-test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class roman_to_integer_test
+    {
+        [Test]
+        public void test()
+        {
+            var target = new roman_to_integer();
+
+            var s = "III";
+            var res = target.RomanToInt(s);
+            Assert.True(res == 3);
+
+            s = "IV";
+            res = target.RomanToInt(s);
+            Assert.True(res == 4);
+
+
+            s = "IX";
+            res = target.RomanToInt(s);
+            Assert.True(res == 9);
+
+            s = "LVIII";
+            res = target.RomanToInt(s);
+            Assert.True(res == 58);
+
+            s = "MCMXCIV";
+            res = target.RomanToInt(s);
+            Assert.True(res == 1994);
+        }
+    }
+}
