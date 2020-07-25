597aa27df8c3b641d30c59a8a7f57172a59bf5df
ManagerSi <smallvv@126.com>
2020-07-26 00:27:08 +0800

    剑指 Offer 28. 对称的二叉树

---
+++ Sort/Leetcode/Problems/剑指offer/dui-cheng-de-er-cha-shu-lcof.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 剑指 Offer 28. 对称的二叉树
+    /// </summary>
+    public class dui_cheng_de_er_cha_shu_lcof
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null )
+                return true;
+
+            if (root.left?.val != root.right?.val)
+                return false;
+
+            return CheckNodes(root.left, root.right);
+        }
+
+        private bool CheckNodes(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+                return true;
+
+            if (left?.val == right?.val)
+            {
+                var res1 = CheckNodes(left.left, right.right);
+                var res2 = CheckNodes(left.right, right.left);
+
+                return res2 && res1;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/dui-cheng-de-er-cha-shu-lcof-test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class dui_cheng_de_er_cha_shu_lcof_test
+    {
+        [Test]
+        public void test_success()
+        {
+            var tClass = new dui_cheng_de_er_cha_shu_lcof();
+            var tree = new TreeNode(1, new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+                new TreeNode(1, new TreeNode(3), new TreeNode(2)));
+
+            var res = tClass.IsSymmetric(tree);
+            Assert.True(res);
+
+
+            res = tClass.IsSymmetric(null);
+            Assert.True(res);
+
+            res = tClass.IsSymmetric(new TreeNode());
+            Assert.True(res);
+        }
+        [Test]
+        public void test_fail()
+        {
+            var tClass = new dui_cheng_de_er_cha_shu_lcof();
+
+            var tree = new TreeNode(1, new TreeNode(1, new TreeNode(2), new TreeNode(5)),
+                new TreeNode(1, new TreeNode(3), new TreeNode(2)));
+            var res = tClass.IsSymmetric(tree);
+            Assert.IsFalse(res);
+
+            tree = new TreeNode(1,new TreeNode(0));
+            res = tClass.IsSymmetric(tree);
+            Assert.IsFalse(res);
+
+        }
+    }
+}
