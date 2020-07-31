c1927c27f31b6b78474c7ea8f70f605a65901345
jerry.si <403563310@qq.com>
2020-07-31 22:48:11 +0800

    230. 二叉搜索树中第K小的元素

---
+++ Sort/Leetcode/Problems/kth-smallest-element-in-a-bst.cs
@@ -0,0 +1,62 @@
+﻿using Leetcode.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 230. 二叉搜索树中第K小的元素
+    /// https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+    /// </summary>
+    public class kth_smallest_element_in_a_bst
+    {
+        /// <summary>
+        /// 中序遍历得到有序数组
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int KthSmallest(TreeNode root, int k)
+        {
+            step = 0;
+            return checkKValue(root, k);
+
+        }
+        private int step = 0;
+        private int checkKValue(TreeNode node, int k)
+        {
+            if (node == null)
+                return int.MinValue;
+
+            //检查左元素
+            var res = checkKValue(node.left, k);
+            if (res != int.MinValue)
+                return res;
+
+            ++step;
+            if (k == step)
+                return node.val;
+
+            //检查右元素
+            res = checkKValue(node.right, k);
+            if (res != int.MinValue)
+                return res;
+
+            return int.MinValue;
+        }
+
+
+        /// <summary>
+        /// 中序遍历得到有序数组
+        /// 使用stack存储
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int KthSmallest_V2(TreeNode root, int k)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/kth-smallest-element-in-a-bst-test.cs
@@ -0,0 +1,27 @@
+﻿using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    class kth_smallest_element_in_a_bst_test
+    {
+        [Test]
+        public void test()
+        {
+            var tClass = new kth_smallest_element_in_a_bst();
+
+            var node = new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4));
+            var res = tClass.KthSmallest(node, 1);
+            Assert.IsTrue(res == 1);
+
+
+            node = new TreeNode(5, new TreeNode(3, new TreeNode(2, new TreeNode(1)), new TreeNode(4)), new TreeNode(6));
+            res = tClass.KthSmallest(node, 3);
+            Assert.IsTrue(res == 3);
+        }
+    }
+}
