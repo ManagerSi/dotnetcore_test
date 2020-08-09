b1c3b6ef2c36413472249fd2c59085c48061a12c
jerry.si <403563310@qq.com>
2020-08-09 22:26:46 +0800

    35. 搜索插入位置

---
+++ Sort/Leetcode/Problems/search-insert-position.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 35. 搜索插入位置
+    /// https://leetcode-cn.com/problems/search-insert-position/
+    /// </summary>
+    public class search_insert_position
+    {
+        /// <summary>
+        /// 暴力破解
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums == null ) return -1;
+            if (nums.Length == 0) return 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < target) continue;
+                if (nums[i] == target)
+                {
+                    return i;
+                }
+                if (nums[i] > target)
+                {
+                    //var arr = new int[nums.Length + 1];
+                    //for (int j = 0; j < i; j++)
+                    //{
+                    //    arr[j] = nums[j];
+                    //}
+                    //arr[i] = target;
+                    //for (int k = i; k < nums.Length; k++)
+                    //{
+                    //    arr[i + 1] = arr[i];
+                    //}
+                    return i;
+                }
+            }
+            return nums.Length;
+        }
+
+        public int SearchInsert_V2(int[] nums, int target)
+        {
+            if (nums == null) return -1;
+            if (nums.Length == 0) return 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < target) continue;
+                if (nums[i] >= target)
+                {
+                    return i;
+                }
+            }
+            return nums.Length;
+        }
+        /// <summary>
+        /// 二分法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int SearchInsert_V3(int[] nums, int target)
+        {
+            int left = 0, right = nums.Length;
+            while (left < right)
+            {
+                var mid = left + (right - left) / 2;
+                if (nums[mid] > target)
+                {
+                    right = mid;
+                }
+                if (nums[mid] == target) return mid;
+                if (nums[mid] < target)
+                {
+                    left = mid+1;
+                }
+            }
+            return left;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/search-insert-position-test.cs
@@ -0,0 +1,55 @@
+﻿using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    class search_insert_position_test
+    {
+        [Test]
+        public void test()
+        {
+            var tClass = new search_insert_position();
+
+            var nums = new int[] { 1, 3, 5, 6 };
+            var res = tClass.SearchInsert(nums, 5);
+            Assert.IsTrue(res == 2);
+
+            nums = new int[] { 1, 3, 5, 6 };
+            res = tClass.SearchInsert(nums, 2);
+            Assert.IsTrue(res == 1);
+
+            nums = new int[] { 1, 3, 5, 6 };
+            res = tClass.SearchInsert(nums, 7);
+            Assert.IsTrue(res == 4);
+
+            nums = new int[] { 1, 3, 5, 6 };
+            res = tClass.SearchInsert(nums, 0);
+            Assert.IsTrue(res == 0);
+        }
+
+        [Test]
+        public void test_V3()
+        {
+            var tClass = new search_insert_position();
+
+            var nums = new int[] { 1, 3, 5, 6 };
+            var res = tClass.SearchInsert_V3(nums, 5);
+            Assert.IsTrue(res == 2);
+
+            nums = new int[] { 1, 3, 5, 6 };
+            res = tClass.SearchInsert_V3(nums, 2);
+            Assert.IsTrue(res == 1);
+
+            nums = new int[] { 1, 3, 5, 6 };
+            res = tClass.SearchInsert_V3(nums, 7);
+            Assert.IsTrue(res == 4);
+
+            nums = new int[] { 1, 3, 5, 6 };
+            res = tClass.SearchInsert_V3(nums, 0);
+            Assert.IsTrue(res == 0);
+        }
+    }
+}
