6f591147acc09afd44d64c8452ef0b51bed6beb5
ManagerSi <smallvv@126.com>
2020-08-17 21:45:35 +0800

    110. Balanced Binary Tree

---
+++ Sort/Leetcode/Problems/balanced-binary-tree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 110. Balanced Binary Tree
+    /// https://leetcode-cn.com/problems/balanced-binary-tree/
+    /// </summary>
+    public class balanced_binary_tree
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null) return true;
+
+            var leftL = TreeHeight(root.left, 0);
+            if (leftL == int.MaxValue) return false;
+
+            var rightL = TreeHeight(root.right, 0);
+            if (rightL == int.MaxValue) return false;
+
+            return Math.Abs(leftL - rightL) <= 1;
+        }
+
+        private int TreeHeight(TreeNode node, int v)
+        {
+            if (node == null)
+                return v;
+            var leftL = TreeHeight(node.left, v + 1);
+            if (leftL == int.MaxValue) return leftL;
+
+            var rightL = TreeHeight(node.right, v + 1);
+            if (rightL == int.MaxValue) return rightL;
+
+            if (Math.Abs(leftL - rightL) > 1)
+                return int.MaxValue;
+
+            return leftL > rightL ? leftL : rightL;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/balanced-binary-tree-test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class balanced_binary_tree_test
+    {
+        [Test]
+        public void test()
+        {
+            var tClass = new balanced_binary_tree();
+
+            var root = new TreeNode(3, new TreeNode(9), new TreeNode(30, new TreeNode(20), new TreeNode(12)));
+            var res = tClass.IsBalanced(root);
+            Assert.IsTrue(res);
+
+
+            //    [1,2,2,3,null,null,3,4,null,null,4]
+
+            root = new TreeNode(3, 
+                    new TreeNode(9,new TreeNode(2,new TreeNode(3),null),null), 
+                    new TreeNode(30, null,new TreeNode(20, null,new TreeNode(12))));
+            res = tClass.IsBalanced(root);
+            Assert.False(res);
+    }
+    }
+}
