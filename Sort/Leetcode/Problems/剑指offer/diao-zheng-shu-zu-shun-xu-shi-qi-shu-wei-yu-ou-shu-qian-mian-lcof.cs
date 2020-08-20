e8dedc3209820365b18aa22f1022946a61fa0ccb
ManagerSi <smallvv@126.com>
2020-08-20 22:00:04 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

---
+++ Sort/Leetcode/Problems/剑指offer/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems.剑指offer
+{
+    /// <summary>
+    /// 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+    /// https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+    /// </summary>
+    public class diao_zheng_shu_zu_shun_xu_shi_qi_shu_wei_yu_ou_shu_qian_mian_lcof
+    {
+        /// <summary>
+        /// 双指针
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] Exchange(int[] nums)
+        {
+            if (nums == null || nums.Length < 2)
+                return nums;
+            int l = 0,r = nums.Length-1;
+            while (l < r)
+            {
+                while (l<r && nums[l] % 2 == 1)//奇数
+                    l++;
+                while (l < r && nums[r] % 2 == 0)
+                    r--;
+
+                if (l < r)
+                {
+                    var temp = nums[l];
+                    nums[l] = nums[r];
+                    nums[r] = temp;
+                    l++;
+                    r--;
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/剑指offer/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof-test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Extend;
+using Leetcode.Problems.剑指offer;
+using NUnit.Framework;
+
+namespace LeetcodeTests.剑指offer
+{
+    class diao_zheng_shu_zu_shun_xu_shi_qi_shu_wei_yu_ou_shu_qian_mian_lcof_test
+    {
+        [Test]
+        public void test()
+        {
+            var tClass = new diao_zheng_shu_zu_shun_xu_shi_qi_shu_wei_yu_ou_shu_qian_mian_lcof();
+
+            var nums = new int[] { 1,2,3,4};
+            var res = tClass.Exchange(nums);
+            Assert.IsTrue(IntExtent.ToString(res) == "1,3,2,4");
+
+            nums = new int[] {1,3,5};
+            res = tClass.Exchange(nums);
+            Assert.IsTrue(IntExtent.ToString(res) == "1,3,5");
+        }
+    }
+}
