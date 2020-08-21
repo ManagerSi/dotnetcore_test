256a57c8d894cee30a3c2216bfc561ed5df12f39
jerry.si <403563310@qq.com>
2020-08-21 21:31:54 +0800

    11. 盛最多水的容器

---
+++ Sort/Leetcode/Problems/container-with-most-water.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 11. 盛最多水的容器
+    /// https://leetcode-cn.com/problems/container-with-most-water/
+    /// </summary>
+    public class container_with_most_water
+    {
+        /// <summary>
+        /// 双指针
+        /// 擦，这是双for
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxArea(int[] height)
+        {
+            if (height == null || height.Length == 1) return 0; 
+
+            int result = 0;
+            for (int i = 0; i < height.Length-1; i++)
+            {
+                for (int j = i+1; j < height.Length; j++)
+                {
+                    var tempV = CalculateV(height, i, j);
+                    if (result < tempV) result = tempV;
+                }
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// 双指针 real
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxArea_V2(int[] height)
+        {
+            if (height == null || height.Length == 1) return 0;
+
+            int result = 0;
+            int left = 0, right = height.Length-1;
+            while(left < right)
+            {
+                var tempV = CalculateV(height, left, right);
+                if (result < tempV) result = tempV;
+
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+
+            }
+            
+            return result;
+        }
+
+
+        /// <summary>
+        /// 计算容积（长*高）
+        /// </summary>
+        /// <param name="height"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private int CalculateV(int[] nums, int i, int j)
+        {
+            var length = Math.Abs(i - j);
+            var heigh = Math.Min(nums[i], nums[j]);
+            return heigh * length;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/container-with-most-water-test.cs
@@ -0,0 +1,27 @@
+﻿using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    class container_with_most_water_test
+    {
+        private container_with_most_water tClass = new container_with_most_water();
+        [Test]
+        public void test()
+        {
+            var nums = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var res = tClass.MaxArea(nums);
+            Assert.IsTrue(res == 49);
+        }
+        [Test]
+        public void test_V2()
+        {
+            var nums = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var res = tClass.MaxArea_V2(nums);
+            Assert.IsTrue(res == 49);
+        }
+    }
+}
