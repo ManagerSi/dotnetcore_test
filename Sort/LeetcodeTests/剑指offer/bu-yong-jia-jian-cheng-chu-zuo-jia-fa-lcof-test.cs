af6301f1289c7c13aa1dee9bcebb144b31383457
jerry.si <403563310@qq.com>
2020-08-23 23:09:30 +0800

    剑指 Offer 65. 不用加减乘除做加法

---
+++ Sort/LeetcodeTests/剑指offer/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof-test.cs
@@ -0,0 +1,24 @@
+﻿using Leetcode.Problems.剑指offer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests.剑指offer
+{
+    class bu_yong_jia_jian_cheng_chu_zuo_jia_fa_lcof_test
+    {
+        private bu_yong_jia_jian_cheng_chu_zuo_jia_fa_lcof tClass = new bu_yong_jia_jian_cheng_chu_zuo_jia_fa_lcof();
+        [Test]
+        public void test()
+        {
+            int a = 10, b = 24;
+            var res = tClass.Add(a, b);
+            Assert.IsTrue(res == a + b);
+
+            a = -1; b =2;
+            res = tClass.Add(a, b);
+            Assert.IsTrue(res == a + b);
+        }
+    }
+}
