dcc819c4e35dfbf2a9c27dc9289831cecdeb03b7
jerry.si <403563310@qq.com>
2020-08-24 21:36:18 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ Sort/Leetcode/Problems/剑指offer/shu-zu-zhong-zhong-fu-de-shu-zi-lcof.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems.剑指offer
+{
+    /// <summary>
+    /// 剑指 Offer 03. 数组中重复的数字
+    /// </summary>
+    public class shu_zu_zhong_zhong_fu_de_shu_zi_lcof
+    {
+        public int FindRepeatNumber(int[] nums)
+        {
+            var set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.Contains(nums[i]))
+                {
+                    return nums[i];
+                }
+                set.Add(nums[i]);
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// 先排序，两两对比
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindRepeatNumber_V2(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i+1])
+                {
+                    return nums[i];
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/剑指offer/shu-zu-zhong-zhong-fu-de-shu-zi-lcof-test.cs
@@ -0,0 +1,25 @@
+﻿using Leetcode.Problems.剑指offer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests.剑指offer
+{
+    class shu_zu_zhong_zhong_fu_de_shu_zi_lcof_test
+    {
+        private shu_zu_zhong_zhong_fu_de_shu_zi_lcof tClass = new shu_zu_zhong_zhong_fu_de_shu_zi_lcof();
+
+        [Test]
+        public void test()
+        {
+            var nums = new int[]{ 1,2,3,4,5,2};
+            var res = tClass.FindRepeatNumber(nums);
+            Assert.IsTrue(new List<int>() { 2 }.Contains(res));
+
+            nums = new int[] { 2, 3, 1, 0, 2, 5, 3 }; 
+            res = tClass.FindRepeatNumber(nums);
+            Assert.IsTrue(new List<int>() { 2, 3 }.Contains(res));
+        }
+    }
+}
