f9b880563140ade6e34490ff436bc6b8c60b1d1a
ManagerSi <smallvv@126.com>
2020-08-27 20:54:24 +0800

    add IntExtent

---
+++ Sort/Leetcode/Extend/IntExtent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Extend
+{
+    public  static  class IntExtent
+    {
+        public static string ToString(this int[] nums)
+        {
+            if (nums == null) return string.Empty;
+            return string.Join(',', nums);
+        }
+    }
+}
