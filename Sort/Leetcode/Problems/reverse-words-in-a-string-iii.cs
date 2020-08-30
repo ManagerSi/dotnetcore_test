21bdf513c80852020904670c7ff2b0eed6b106a7
ManagerSi <smallvv@126.com>
2020-08-30 22:39:13 +0800

    557. 反转字符串中的单词 III

---
+++ Sort/Leetcode/Problems/reverse-words-in-a-string-iii.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 557. 反转字符串中的单词 III
+    /// https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
+    /// </summary>
+    public class reverse_words_in_a_string_iii
+    {
+        /// <summary>
+        /// use string libary
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return String.Empty;
+            }
+
+            string[] spliteList = s.Trim().Split(" ").Where(i=>!string.IsNullOrEmpty(i)).ToArray();
+
+            for (int i = 0; i < spliteList.Length; i++)
+            {
+                var s1 = spliteList[i];
+                spliteList[i] = string.Join("", s1.Reverse());
+            }
+
+            return string.Join(" ", spliteList);
+        }
+
+
+        /// <summary>
+        /// use string libary
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string ReverseWords_V2(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return String.Empty;
+            }
+
+            string[] spliteList = s.Trim().Split(" ");
+
+            for (int i = 0; i < spliteList.Length; i++)
+            {
+                var s1 = spliteList[i];
+                spliteList[i] = string.Join("", s1.Reverse());
+            }
+
+            return string.Join(" ", spliteList);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/reverse-words-in-a-string-iii-test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class reverse_words_in_a_string_iii_test
+    {
+        private reverse_words_in_a_string_iii target = new reverse_words_in_a_string_iii();
+
+        [Test]
+        public void test()
+        {
+            string s = "Let's take LeetCode contest";
+            var res = target.ReverseWords(s);
+            Assert.True(res == "s'teL ekat edoCteeL tsetnoc");
+
+            //s is empty
+            s = "";
+            res = target.ReverseWords(s);
+            Assert.True(res == "");
+
+
+            //space at start
+            s = " sd sa";
+            res = target.ReverseWords(s);
+            Assert.True(res == "ds as");
+        }
+    }
+}
