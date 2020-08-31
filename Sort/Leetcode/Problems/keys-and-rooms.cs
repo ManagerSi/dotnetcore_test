5f5fbcbd767edf3c69649562d1860950bc2add7d
ManagerSi <smallvv@126.com>
2020-08-31 22:41:09 +0800

    841. 钥匙和房间

---
+++ Sort/Leetcode/Problems/keys-and-rooms.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 841. 钥匙和房间
+    /// https://leetcode-cn.com/problems/keys-and-rooms/
+    /// </summary>
+    public class keys_and_rooms
+    {
+        /// <summary>
+        /// 深度优先遍历
+        /// </summary>
+        /// <param name="rooms"></param>
+        /// <returns></returns>
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            var res = Enumerable.Range(0, rooms.Count).ToDictionary(i => i, i => false);
+
+            dfs(rooms, 0, res);
+
+            return !(res.Any(i => i.Value == false));
+        }
+
+        private void dfs(IList<IList<int>> rooms, int v, Dictionary<int, bool> res)
+        {
+            if (res[v])//房间已进入则无需再次进入
+                return;
+
+            //设置房间已进入
+            res[v] = true;
+
+            var keysInRoom = rooms[v];
+
+            foreach (var roomIndex in keysInRoom)
+            {
+                dfs(rooms, roomIndex, res);
+
+            }
+
+        }
+
+        /// <summary>
+        /// 深度优先遍历,采用bool数组
+        /// </summary>
+        /// <param name="rooms"></param>
+        /// <returns></returns>
+        public bool CanVisitAllRooms_V2(IList<IList<int>> rooms)
+        {
+            var res = new bool[rooms.Count];
+
+            dfs(rooms, 0, res);
+
+            return !(res.Any(i => i == false));
+        }
+
+        private void dfs(IList<IList<int>> rooms, int v, bool[] res)
+        {
+            if (res[v])//房间已进入则无需再次进入
+                return;
+
+            //设置房间已进入
+            res[v] = true;
+
+            var keysInRoom = rooms[v];
+
+            foreach (var roomIndex in keysInRoom)
+            {
+                dfs(rooms, roomIndex, res);
+
+            }
+
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/keys-and-rooms-test.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class keys_and_rooms_test
+    {
+        private keys_and_rooms target = new keys_and_rooms();
+
+        [Test]
+        public void test()
+        {
+            var rooms = new List<IList<int>>()
+            {
+                new List<int>(){1},
+                new List<int>(){2},
+                new List<int>(){3},
+                new List<int>(){},
+            };
+            var res = target.CanVisitAllRooms(rooms);
+            Assert.True(res);
+
+            //false
+            rooms = new List<IList<int>>()
+            {
+                new List<int>(){1,3},
+                new List<int>(){3,0,1},
+                new List<int>(){2},
+                new List<int>(){0},
+            };
+            res = target.CanVisitAllRooms(rooms);
+            Assert.False(res);
+        }
+
+        [Test]
+        public void test_V2()
+        {
+            var rooms = new List<IList<int>>()
+            {
+                new List<int>(){1},
+                new List<int>(){2},
+                new List<int>(){3},
+                new List<int>(){},
+            };
+            var res = target.CanVisitAllRooms_V2(rooms);
+            Assert.True(res);
+
+            //false
+            rooms = new List<IList<int>>()
+            {
+                new List<int>(){1,3},
+                new List<int>(){3,0,1},
+                new List<int>(){2},
+                new List<int>(){0},
+            };
+            res = target.CanVisitAllRooms_V2(rooms);
+            Assert.False(res);
+        }
+    }
+}
