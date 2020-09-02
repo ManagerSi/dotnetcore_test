16af790e79615fa57b579411b86c82aafa1e05e8
ManagerSi <smallvv@126.com>
2020-09-02 22:38:02 +0800

    剑指 Offer 20. 表示数值的字符串

---
+++ Sort/Leetcode/Problems/剑指offer/biao-shi-shu-zhi-de-zi-fu-chuan-lcof.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Leetcode.Problems.剑指offer
+{
+    /// <summary>
+    /// 剑指 Offer 20. 表示数值的字符串
+    /// https://leetcode-cn.com/problems/biao-shi-shu-zhi-de-zi-fu-chuan-lcof/
+    /// </summary>
+    public class biao_shi_shu_zhi_de_zi_fu_chuan_lcof
+    {
+        public bool IsNumber(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+
+            if (decimal.TryParse(s, out var num))
+                return true;
+
+            try
+            {
+                Single.Parse(s);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return false;
+
+        }
+
+        /// <summary>
+        /// 正则
+        /// 这题简直就是：试用例猜规则。 现在基本可以明确规则如下：
+        /// 
+        /// 数字两边可以有空格，但是中间插空格不行；
+        /// 除了数字之外，合法字符还有'e'、'E'、'+'、'-'、'.'；
+        /// 'e'、'E'等价，用来划分底数与指数，只能出现一次，前面为科学计数法的底数，后面为指数；
+        /// '+'、'-'只能作为正负号，但是不可以作为加减号，也就是只能出现在底数和指数的前面，不能出现在两个数字中间；
+        /// '.'只能在底数上，不能在指数上，且只能出现一次，'.'两边任一边有数字均算一个完整的数字，但单独一个'.'不行。
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsNumber_V2(string s)
+        {
+            var pattern = "^[+-]?(((\\d+\\.?)|(\\.\\d+))|(\\d+\\.\\d+))([eE][+-]?\\d+)?$";//"^[+-]?((\\d*)|(\\d+.)|(.\\d+))([eE][+-]?\\d+)?$"; //^[+-]?(((\\d+\\.?)|(\\.\\d+))|(\\d+\\.\\d+))([eE][+-]?\\d+)?$
+            if (Regex.IsMatch(s.Trim(), pattern))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// 状态机
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsNumber_V3(string s)
+        {
+            return false;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/剑指offer/biao-shi-shu-zhi-de-zi-fu-chuan-lcof-test.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems.剑指offer;
+using NUnit.Framework;
+
+namespace LeetcodeTests.剑指offer
+{
+    class biao_shi_shu_zhi_de_zi_fu_chuan_lcof_test
+    {
+        private biao_shi_shu_zhi_de_zi_fu_chuan_lcof target = new biao_shi_shu_zhi_de_zi_fu_chuan_lcof();
+
+        /// <summary>
+        /// ".8+" 不通过
+        /// </summary>
+        [Test]
+        public void test()
+        {
+            var trueStr = new string[]{
+                "+100","5e2","-123","3.1416","-1E-16","0123"
+            };
+            foreach (var s in trueStr)
+            {
+                var res = target.IsNumber(s);
+                Assert.True(res);
+            }
+
+            var falseStr = new string[] {  "12e", "1a3.14", "1.2.3", "+-5", "12e+5.4"}; 
+            foreach (var s in falseStr)
+            {
+                var res = target.IsNumber(s);
+                Assert.False(res);
+            }
+        }
+
+
+        [Test]
+        public void test_V2()
+        {
+            var trueStr = new string[]{ "1 ","+100","5e2","-123","3.1416","-1E-16","0123"
+            };
+            foreach (var s in trueStr)
+            {
+                var res = target.IsNumber_V2(s);
+                Assert.True(res);
+            }
+
+            var falseStr = new string[] { ".8+", "12e", "1a3.14", "1.2.3", "+-5", "12e+5.4" };
+            foreach (var s in falseStr)
+            {
+                var res = target.IsNumber_V2(s);
+                Assert.False(res);
+            }
+        }
+    }
+}
