b5b52357cbb24b5a5dc03ed83c5dc0289062c0f6
ManagerSi <smallvv@126.com>
2020-09-12 23:12:09 +0800

    637. 二叉树的层平均值

---
+++ Sort/Leetcode/Problems/average-of-levels-in-binary-tree.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 637. 二叉树的层平均值
+    /// https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+    /// </summary>
+    public class average_of_levels_in_binary_tree
+    {
+        /// <summary>
+        /// 层次遍历--递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            result = new List<double>();
+
+            List<TreeNode> treeList = new List<TreeNode>(){root};
+            Leval(treeList);
+
+            return result;
+        }
+
+        private List<double> result;
+
+        private void Leval(List<TreeNode> treeList)
+        {
+            if (treeList == null|| treeList.Count==0)
+                return;
+
+            List<TreeNode> tempList = new List<TreeNode>();
+            double levelSum = 0;
+            foreach (var node in treeList)
+            {
+                levelSum += node.val;
+
+                if(node.left!=null)
+                    tempList.Add(node.left);
+                if (node.right != null)
+                    tempList.Add(node.right);
+            }
+
+            result.Add(levelSum/(double)treeList.Count);
+
+            Leval(tempList);
+        }
+
+
+        /// <summary>
+        /// 层次遍历--非递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<double> AverageOfLevels_V2(TreeNode root)
+        {
+            var result1 = new List<double>();
+            if (root == null)
+                return result1;
+
+            List<TreeNode> treeList = new List<TreeNode>() { root };
+            List<TreeNode> tempList = new List<TreeNode>();
+            while (treeList.Count>0)
+            {
+                double levelSum = 0;
+                foreach (var node in treeList)
+                {
+                    levelSum += node.val;
+
+                    if (node.left != null)
+                        tempList.Add(node.left);
+                    if (node.right != null)
+                        tempList.Add(node.right);
+                }
+
+                result1.Add(levelSum / (double)treeList.Count);
+
+                treeList.Clear();
+                treeList.AddRange(tempList);
+                tempList = new List<TreeNode>();
+            }
+
+            return result1;
+        }
+
+
+        /// <summary>
+        /// 层次遍历--非递归--队列
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<double> AverageOfLevels_V3(TreeNode root)
+        {
+            var result1 = new List<double>();
+            if (root == null)
+                return result1;
+
+            Queue<TreeNode> treeQueue = new Queue<TreeNode>();
+            treeQueue.Enqueue(root);
+
+            double levelSum = 0;
+            int count = 0;
+            while (treeQueue.Count > 0)
+            {
+                levelSum = 0;
+                count = treeQueue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var node = treeQueue.Dequeue();
+
+                    levelSum += node.val;
+
+                    if (node.left != null)
+                        treeQueue.Enqueue(node.left);
+                    if (node.right != null)
+                        treeQueue.Enqueue(node.right);
+                }
+
+                result1.Add(levelSum / (double)count);
+            }
+
+            return result1;
+        }
+
+    }
+}
---
+++ Sort/LeetcodeTests/average-of-levels-in-binary-tree-test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class average_of_levels_in_binary_tree_test
+    {
+        average_of_levels_in_binary_tree target = new average_of_levels_in_binary_tree();
+
+        [Test]
+        public void test()
+        {
+            var tree = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var res = target.AverageOfLevels(tree);
+            Assert.True(res.Count == 3);
+        }
+
+        [Test]
+        public void test_v2()
+        {
+            var tree = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var res = target.AverageOfLevels_V2(tree);
+            Assert.True(res.Count == 3);
+        }
+
+        [Test]
+        public void test_v3()
+        {
+            var tree = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            var res = target.AverageOfLevels_V3(tree);
+            Assert.True(res.Count == 3);
+        }
+    }
+}
