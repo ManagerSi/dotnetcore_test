bc3e2edca0e1822450d81efc43c57f3adebf9326
ManagerSi <smallvv@126.com>
2020-09-15 00:52:24 +0800

    增加MyArray方法及单元测试

--- Sort/DataStructures/MyArray.cs
+++ Sort/DataStructures/MyArray.cs
@@ -11,20 +11,19 @@ namespace DataStructures
 
         public MyArray()
         {
-            _array = new T[]();
             _size = 0;
+            _array = new T[_size];
         }
 
         public MyArray(int length)
         {
-            _array = new T[length]();
+            _array = new T[length];
             _size = length;
         }
 
         public int Length
         {
-            get { return _leanth;}
-            private set;
+            get { return _size;}
         }
 
         /// <summary>
@@ -38,6 +37,137 @@ namespace DataStructures
             set { _array[index] = value; }
         }
 
+        public void Add(T data)
+        {
+            T[] tempArray = new T[_size+1];
+            for (int i = 0; i < _size; i++)
+            {
+                tempArray[i] = _array[i];
+            }
+            tempArray[_size] = data;
 
+            _array = null;
+            _array = tempArray;
+            _size++;
+
+            tempArray = null;
+        }
+
+        public void Insert(int index, T data)
+        {
+            if (index >= _size)
+                Add(data);
+            else
+            {
+
+                T[] tempArray = new T[_size + 1];
+                for (int i = 0; i < index; i++)
+                {
+                    tempArray[i] = _array[i];
+                }
+                tempArray[index] = data;
+                for (int i = index+1; i <= _size; i++)
+                {
+                    tempArray[i] = _array[i-1];
+                }
+
+                _array = null;
+                _array = tempArray;
+                _size++;
+
+                tempArray = null;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _size)
+                return;
+
+            _size--;
+            for (int i = index; i < _size; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+
+            _array[_size] = default(T);
+
+        }
+
+        public void Remove<T>(T data) where T : IComparable
+        {
+            for (int i = 0; i < _size ; i++)
+            {
+                if (data.CompareTo(_array[i])==0)
+                    RemoveAt(i);
+            }
+        }
+
+        public void RemoveAll()
+        {
+            _size = 0;
+            _array=new T[_size];
+        }
+
+        public override string ToString()
+        {
+            if(_size>0)
+                return string.Join(',',_array);
+            return String.Empty;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+
+        public static void Clear(T[] array) { }
+
+        public static int IndexOf(T[] array, T data)
+        {
+            //todo implation
+
+            return 0;
+        }
+        public static int LastIndexOf(T[] array, T data)
+        {
+            //todo implation
+
+            return 0;
+        }
+        public static T[] Copy(T[] array)
+        {
+            //todo implation
+
+            return array;
+        }
+
+        public static T[] Reverse(T[] array)
+        {
+            //todo implation
+
+            return array;
+        }
+
+        public static T[] QuickSort(T[] array)
+        {
+            //todo implation
+
+            return array;
+        }
+
+        public static T[] BinarySort(T[] array)
+        {
+            //todo implation
+
+            return array;
+        }
     }
 }
---
+++ Sort/Tests/DataStructuresTests/MyArrayTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures;
+using NUnit.Framework;
+
+namespace Tests.DataStructuresTests
+{
+    class MyArrayTests
+    {
+        MyArray<int> target;
+
+        [SetUp]
+        public void setup()
+        {
+            target = new MyArray<int>(5);
+            for (int i = 0; i < 5; i++)
+            {
+                target[i] = i;
+            }
+        }
+
+        [Test]
+        public void Add_test()
+        {
+            target.Add(5);
+            Assert.True(target.Length == 6);
+            Assert.True(target[5] == 5);
+        }
+
+        [Test]
+        public void Insert_test()
+        {
+            target.Insert(1,5);
+            Assert.True(target.Length == 6);
+            Assert.True(target.ToString() == "0,5,1,2,3,4");
+        }
+
+        [Test]
+        public void RemoveAt_test()
+        {
+            target.RemoveAt(1);
+            Assert.True(target.Length == 4);
+            Assert.True(target.ToString() == "0,2,3,4,0");
+        }
+
+        [Test]
+        public void Remove_test()
+        {
+            target.Remove(1);
+            Assert.True(target.Length == 4);
+            Assert.True(target.ToString() == "0,2,3,4,0");
+        }
+
+        [Test]
+        public void RemoveAll_test()
+        {
+            target.RemoveAll();
+            Assert.True(target.Length == 0);
+            Assert.True(target.ToString() == "");
+        }
+
+
+    }
+}
