a372206c39e89d05de5e76eccdb2a1d233ad044b
ManagerSi <smallvv@126.com>
2020-09-23 00:09:17 +0800

    27. 移除元素

---
+++ Sort/Leetcode/Problems/remove-element.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 27. 移除元素
+    /// https://leetcode-cn.com/problems/remove-element/
+    /// </summary>
+    public class remove_element
+    {
+        /// <summary>
+        /// 双指针
+        /// 当我们遇到 nums[i] = valnums[i]=val 时，我们可以将当前元素与最后一个元素进行交换，并释放最后一个元素。这实际上使数组的大小减少了 1。
+        /// 请注意，被交换的最后一个元素可能是您想要移除的值。但是不要担心，在下一次迭代中，我们仍然会检查这个元素。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public int RemoveElement(int[] nums, int val)
+        {
+            int i = 0;
+            int n = nums.Length;
+            while (i < n)
+            {
+                if (nums[i] == val)
+                {
+                    nums[i] = nums[n - 1];
+                    // reduce array size by one
+                    n--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return n;
+        }
+        /// <summary>
+        /// 拷贝覆盖
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public int RemoveElement_V2(int[] nums, int val)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            var valLength = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[valLength++] = nums[i];
+                }
+            }
+
+            return valLength;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/remove-element-test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class remove_element_test
+    {
+        remove_element target = new remove_element();
+
+        [Test]
+        public void test()
+        {
+            var nums = new int[] { 3, 2, 2, 3 };
+            var t = 2;
+            var res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 2);
+
+            nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            t = 2;
+            res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 5);
+
+            nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            t = 5;
+            res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 8);
+
+            nums = new int[] { };
+            t = 5;
+            res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 0);
+        }
+
+        [Test]
+        public void test_V2()
+        {
+            var nums = new int[] { 3, 2, 2, 3 };
+            var t = 2;
+            var res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 2);
+
+            nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            t = 2;
+            res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 5);
+
+            nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            t = 5;
+            res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 8);
+
+            nums = new int[] {  };
+            t = 5;
+            res = target.RemoveElement_V2(nums, t);
+            Assert.True(res == 0);
+        }
+    }
+}
