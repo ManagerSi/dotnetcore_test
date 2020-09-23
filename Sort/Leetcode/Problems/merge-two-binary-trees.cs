8bd3305ec77d5dc7f67bb836c6da50c672d94890
ManagerSi <smallvv@126.com>
2020-09-23 23:55:22 +0800

    617. 合并二叉树

---
+++ Sort/Leetcode/Problems/merge-two-binary-trees.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 617. 合并二叉树
+    /// https://leetcode-cn.com/problems/merge-two-binary-trees/
+    /// </summary>
+    public class merge_two_binary_trees
+    {
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+           return DFS(t1, t2);
+        }
+
+        private TreeNode DFS(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null)
+            {
+                return null;
+            }
+
+            var newRoot = new TreeNode((t1?.val??0) + (t2?.val ?? 0));
+
+            newRoot.left = DFS(t1?.left, t2?.left);
+
+            newRoot.right = DFS(t1?.right, t2?.right);
+            
+            return newRoot;
+        }
+
+
+        public TreeNode MergeTrees_V2(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null) return t2;
+
+            if (t2 == null) return t1;
+
+            var newRoot = new TreeNode((t1?.val ?? 0) + (t2?.val ?? 0));
+
+            newRoot.left = DFS(t1?.left, t2?.left);
+
+            newRoot.right = DFS(t1?.right, t2?.right);
+
+            return newRoot;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/merge-two-binary-trees-test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class merge_two_binary_trees_test
+    {
+        merge_two_binary_trees target = new merge_two_binary_trees();
+
+        [Test]
+        public void test()
+        {
+            var t1 = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2));
+            var t2 = new TreeNode(2,new TreeNode(1,null,new TreeNode(4)),new TreeNode(3,null,new TreeNode(7)));
+            var result = target.MergeTrees(t1, t2);
+            Assert.IsTrue(result.ToInorderString() == "5,4,4,3,5,7");
+
+            t1 = null;
+            t2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+            result = target.MergeTrees(t1, t2);
+            Assert.IsTrue(result.ToInorderString() == "1,4,2,3,7");
+
+            t1 = null;
+            t2 = null;
+            result = target.MergeTrees(t1, t2);
+            Assert.IsTrue(result?.ToInorderString() == null);
+        }
+
+        [Test]
+        public void test_V2()
+        {
+            var t1 = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2));
+            var t2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+            var result = target.MergeTrees_V2(t1, t2);
+            Assert.IsTrue(result.ToInorderString() == "5,4,4,3,5,7");
+
+            t1 = null;
+            t2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+            result = target.MergeTrees_V2(t1, t2);
+            Assert.IsTrue(result.ToInorderString() == "1,4,2,3,7");
+
+            t1 = null;
+            t2 = null;
+            result = target.MergeTrees_V2(t1, t2);
+            Assert.IsTrue(result?.ToInorderString() == null);
+        }
+    }
+}
