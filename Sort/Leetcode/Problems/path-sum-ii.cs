e3216a80eff32b37744fcef71d8e9ed3c514c53a
ManagerSi <smallvv@126.com>
2020-09-26 22:44:27 +0800

    113. 路径总和 II 1.dfs方法接替

---
+++ Sort/Leetcode/Problems/path-sum-ii.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 113. 路径总和 II
+    /// https://leetcode-cn.com/problems/path-sum-ii/
+    /// </summary>
+    public class path_sum_ii
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+            resultList = new List<IList<int>>();
+            path = new List<int>();
+
+            DFS(root, sum);
+
+            return resultList;
+        }
+        private IList<IList<int>> resultList = null;
+        private List<int> path = null;
+
+        private void DFS(TreeNode root, int sum)
+        {
+            //退出条件
+            if (root == null) return;
+
+            //计算根,加入path
+            var subtraction = sum - root.val;
+            path.Add(root.val);
+
+            if (root.left == null && root.right == null && subtraction == 0)
+            {
+                resultList.Add(path.GetRange(0, path.Count));
+            }
+
+            //计算左子树
+            DFS(root.left, subtraction);
+
+            //计算右子树
+            DFS(root.right, subtraction);
+
+            //删除自己
+            path.RemoveAt(path.Count - 1);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/path-sum-ii-test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class path_sum_ii_test
+    {
+        path_sum_ii target = new path_sum_ii();
+
+        [Test]
+        public void test()
+        {
+            TreeNode root = 
+                new TreeNode(5,
+                 new TreeNode(4,
+                             new TreeNode(11,new TreeNode(7),new TreeNode(2)),
+                            null),
+                new TreeNode(8,
+                             new TreeNode(13),
+                            new TreeNode(4,new TreeNode(5),new TreeNode(1))));
+
+            var res = target.PathSum(root, 22);
+            Assert.IsTrue(res.Count == 2);
+
+
+            root =null;
+            res = target.PathSum(root, 22);
+            Assert.IsTrue(res.Count == 0);
+
+
+            root = new TreeNode(2,new TreeNode(2));
+            res = target.PathSum(root, 0);
+            Assert.IsTrue(res.Count == 0);
+
+            root = new TreeNode(-2, null,new TreeNode(-3));
+            res = target.PathSum(root, -5);
+            Assert.IsTrue(res.Count == 1);
+        }
+    }
+}
