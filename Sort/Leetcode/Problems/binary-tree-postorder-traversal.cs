bcdb95f4186c6f113299f2a70c4ddbc6b0df6a5d
ManagerSi <smallvv@126.com>
2020-09-30 01:32:05 +0800

    145. 二叉树的后序遍历

---
+++ Sort/Leetcode/Problems/binary-tree-postorder-traversal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 145. 二叉树的后序遍历
+    /// https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+    /// </summary>
+    public class binary_tree_postorder_traversal
+    {
+        private List<int> orderList = null;
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            orderList = new List<int>();
+            PostOrder(root);
+            return orderList;
+        }
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="root"></param>
+        private void PostOrder(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            if(root.left!=null)
+                PostOrder(root.left);
+
+            if (root.right != null)
+                PostOrder(root.right);
+
+            orderList.Add(root.val);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/binary-tree-postorder-traversal-test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class binary_tree_postorder_traversal_test
+    {
+        public binary_tree_postorder_traversal target = new binary_tree_postorder_traversal();
+
+        [Test]
+        public void test()
+        {
+            TreeNode node = new TreeNode(1,null,new TreeNode(2,new TreeNode(3),null));
+            var res = target.PostorderTraversal(node);
+            Assert.IsTrue(string.Join(',',res) == node.ToPostorderString());
+
+            node = null;
+            res = target.PostorderTraversal(node);
+            Assert.IsTrue(string.Join(',', res) == "");
+        }
+    }
+}
