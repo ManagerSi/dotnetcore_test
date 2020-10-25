d8713349e1982ec0c8c7785ff69ee2198b6001a9
jerry.si <403563310@qq.com>
2020-10-25 22:21:08 +0800

    845. 数组中的最长山脉

---
+++ Sort/Leetcode/Problems/longest-mountain-in-array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 845. 数组中的最长山脉
+    /// https://leetcode-cn.com/problems/longest-mountain-in-array/
+    /// </summary>
+    public class longest_mountain_in_array
+    {
+        public int LongestMountain(int[] A)
+        {
+            if (A == null || A.Length < 3)
+                return 0;
+
+            int maxMountain = 0;
+            int increasing = 0, decreasing = 0;
+            for (int i = 1; i < A.Length;)
+            {
+                increasing = 0; decreasing = 0;
+
+                while (i < A.Length && A[i-1] < A[i]) { increasing++; i++; };
+                while (i < A.Length && A[i-1] > A[i]) { decreasing++; i++; };
+
+                if (increasing >= 1 && decreasing >= 1)
+                    maxMountain = Math.Max(maxMountain, increasing + decreasing + 1);
+                
+                while (i < A.Length && A[i - 1] == A[i])
+                    i++;//避免出现相等的情况
+            }
+
+            return maxMountain;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/longest-mountain-in-array-test.cs
@@ -0,0 +1,23 @@
+﻿using Leetcode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class longest_mountain_in_array_test
+    {
+        longest_mountain_in_array target = new longest_mountain_in_array();
+
+        [Test]
+        [TestCase(new int[] { 2, 1, 4, 7, 3, 2, 5 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 7, 3, 2 }, ExpectedResult = 0)]
+        [TestCase(new int[] { 1, 2, 3 }, ExpectedResult = 0)]
+        [TestCase(new int[] { 2,2,2 }, ExpectedResult = 0)]
+        public int test(int[] input)
+        {
+            return target.LongestMountain(input);
+        }
+    }
+}
