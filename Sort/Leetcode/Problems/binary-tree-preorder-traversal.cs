e1c517bc34cce576d1ca1af92b7149ccb053d913
ManagerSi <smallvv@126.com>
2020-10-27 22:11:29 +0800

    144. 二叉树的前序遍历

---
+++ Sort/Leetcode/Problems/binary-tree-preorder-traversal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 144. 二叉树的前序遍历
+    /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+    /// </summary>
+    public class binary_tree_preorder_traversal
+    {
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var res = new List<int>();
+            if (root == null)
+                return res;
+
+            PreOrder(root, res);
+
+            return res;
+        }
+
+        private void PreOrder(TreeNode root, List<int> res)
+        {
+            if (root == null)
+                return;
+
+            res.Add(root.val);
+
+            PreOrder(root.left, res);
+            PreOrder(root.right, res);
+        }
+
+        /// <summary>
+        /// 迭代
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<int> PreorderTraversal_V2(TreeNode root)
+        {
+            var res = new List<int>();
+            if (root == null)
+                return res;
+
+            Stack<TreeNode> stack=new Stack<TreeNode>();
+            var tempH = root;
+            while (tempH != null || stack.Count>0)
+            {
+                while (tempH != null)
+                {
+                    res.Add(tempH.val);
+
+                    stack.Push(tempH);
+
+                    tempH = tempH.left;
+                }
+
+                var top = stack.Pop();
+                tempH = top.right;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/binary-tree-preorder-traversal-test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class binary_tree_preorder_traversal_test
+    {
+        private binary_tree_preorder_traversal target = new binary_tree_preorder_traversal();
+
+        static IEnumerable<TestCaseData> geTestCaseDatas
+        {
+            get
+            {
+                yield return new TestCaseData(new TreeNode(1,null,new TreeNode(2,new TreeNode(3)))).Returns("1,2,3");
+                yield return new TestCaseData(null).Returns("");
+            }
+        }
+
+
+        [Test]
+        [TestCaseSource("geTestCaseDatas")]
+        //[TestCase(new TreeNode(2), ExpectedResult = null)]
+        public string test(TreeNode root)
+        {
+            var res = target.PreorderTraversal(root);
+
+            return string.Join(',',res);
+        }
+
+
+
+        public class testcasedate
+        {
+            static IEnumerable<TestCaseData> geTestCaseDatas
+            {
+                get
+                {
+                    yield return new TestCaseData(new TreeNode(1, null, new TreeNode(2, new TreeNode(3)))).Returns("1,2,3");
+                    yield return new TestCaseData(null).Returns("");
+                }
+            }
+        }
+        [Test]
+        [TestCaseSource(typeof(testcasedate), "geTestCaseDatas")]
+        //[TestCase(new TreeNode(2), ExpectedResult = null)]
+        public string test_V2(TreeNode root)
+        {
+            var res = target.PreorderTraversal_V2(root);
+
+            return string.Join(',', res);
+        }
+    }
+}
