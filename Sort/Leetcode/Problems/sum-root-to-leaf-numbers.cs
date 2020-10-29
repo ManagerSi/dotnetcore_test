d1dffcff38ea2a2f8498c132e61e90068e1ccb6e
ManagerSi <smallvv@126.com>
2020-10-29 22:07:58 +0800

    129. 求根到叶子节点数字之和

---
+++ Sort/Leetcode/Problems/sum-root-to-leaf-numbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 129. 求根到叶子节点数字之和
+    /// https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/
+    /// </summary>
+    public class sum_root_to_leaf_numbers
+    {
+        /// <summary>
+        /// DFS 深度优先遍历
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int SumNumbers(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            _result = 0;
+            DFS(root,0);
+
+            return _result;
+        }
+
+        private int _result = 0;
+        private void DFS(TreeNode root, int depSum)
+        {
+            //出口
+            if (root == null) return;
+
+            //验证
+            if (root.left == null && root.right == null)
+            {
+                _result += depSum * 10 + root.val;
+                return;
+            }
+
+            //下一个循环
+            DFS(root.left, depSum * 10 + root.val);
+            DFS(root.right, depSum * 10 + root.val);
+        }
+
+    }
+}
---
+++ Sort/LeetcodeTests/sum-root-to-leaf-numbers-test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class sum_root_to_leaf_numbers_test
+    {
+        sum_root_to_leaf_numbers target = new sum_root_to_leaf_numbers();
+
+        private static class TestCasesProvider
+        {
+            public static IEnumerable<TestCaseData> Cases()
+            {
+                yield return new TestCaseData(new TreeNode()).Returns(0);
+                yield return new TestCaseData(new TreeNode()).Returns(0);
+                yield return new TestCaseData(new TreeNode(1,new TreeNode(2),new TreeNode(3))).Returns(25);
+                yield return new TestCaseData(new TreeNode(4,
+                    new TreeNode(9,new TreeNode(5),new TreeNode(1)),
+                    new TreeNode(0))).Returns(1026);
+            }
+        }
+
+        [Test,TestCaseSource(typeof(TestCasesProvider), nameof(TestCasesProvider.Cases))]
+        public int test(TreeNode node)
+        {
+            return target.SumNumbers(node);
+        }
+    }
+}
