b66502ed1baf472e43d988d1c6fd077e828bb4c3
ManagerSi <smallvv@126.com>
2020-11-02 23:23:46 +0800

    349. 两个数组的交集

---
+++ Sort/Leetcode/Problems/intersection-of-two-arrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 349. 两个数组的交集
+    /// https://leetcode-cn.com/problems/intersection-of-two-arrays/
+    /// </summary>
+    public class intersection_of_two_arrays
+    {
+        /// <summary>
+        /// 默认函数
+        /// </summary>
+        /// <param name="nums1"></param>
+        /// <param name="nums2"></param>
+        /// <returns></returns>
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            if (nums1 == null || nums1.Length == 0) return new int[]{};
+            if (nums2 == null || nums2.Length == 0) return new int[] { };
+
+            return nums1.Intersect(nums2).ToArray();
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/intersection-of-two-arrays-test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class intersection_of_two_arrays_test
+    {
+        intersection_of_two_arrays target = new intersection_of_two_arrays();
+
+        [Test]
+        [TestCase(null, new int[] { 2, 2 }, ExpectedResult = new int[] { })]
+        [TestCase(new int[] {  }, new int[] { 2, 2 }, ExpectedResult = new int[] { })]
+        [TestCase(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 },ExpectedResult = new int[] { 2 })]
+        [TestCase(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }, ExpectedResult = new int[] { 4 ,9 })]
+        public int[] test(int[] n1, int[] n2)
+        {
+            return  target.Intersection(n1, n2).OrderBy(i=>i).ToArray();
+        }
+    }
+}
