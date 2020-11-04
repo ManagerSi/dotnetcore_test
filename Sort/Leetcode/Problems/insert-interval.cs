a06955613ab0c5e0072fb9dec7d24e7736db4032
ManagerSi <smallvv@126.com>
2020-11-04 22:44:27 +0800

    57. 插入区间

---
+++ Sort/Leetcode/Problems/insert-interval.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 57. 插入区间
+    /// https://leetcode-cn.com/problems/insert-interval/
+    /// </summary>
+    public class insert_interval
+    {
+        public int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            if(intervals==null || intervals.Length==0)
+                return new int[][]{newInterval};
+
+            List<int[]> result = new List<int[]>();
+            int[] outInterval = new int[] { };
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                bool isInterval = CheckInterval(intervals[i], newInterval, out outInterval);
+                if (isInterval)
+                {
+                    newInterval = outInterval;
+                    continue;
+                }
+                else
+                {
+                    //没有新区间
+                    if (newInterval == null) 
+                    { 
+                        result.Add(intervals[i]);
+                        continue;
+                    }
+
+                    //newInterval 小于当前区间
+                    if (intervals[i][0] > newInterval[1])
+                    {
+                        result.Add(newInterval);
+                        result.Add(intervals[i]);
+                        newInterval = null;
+                        continue;
+                    }
+
+                    //newInterval 大于当前区间
+                    result.Add(intervals[i]);
+                }
+            }
+
+            //新区间在最后
+            if (newInterval != null)
+            {
+                result.Add(newInterval);
+            }
+
+            return result.ToArray();
+        }
+
+        private bool CheckInterval(int[] v, int[] newInterval, out int[] outInterval)
+        {
+            outInterval = new[] {0, 0};
+            if (newInterval == null) return false;
+
+            if (newInterval[0] <= v[1] && newInterval[1] >= v[0] //新区间在左
+                || v[0] <= newInterval[1] && v[1] >= newInterval[0]) //老区间在左
+            {
+                outInterval[0] = Math.Min(v[0], newInterval[0]);
+                outInterval[1] = Math.Max(v[1], newInterval[1]);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// https://leetcode-cn.com/problems/insert-interval/solution/bi-xu-miao-dong-li-kou-qu-jian-ti-mu-zhong-die-qu-/
+        /// </summary>
+        /// <param name="intervals"></param>
+        /// <param name="newInterval"></param>
+        /// <returns></returns>
+        public int[][] Insert_V2(int[][] intervals, int[] newInterval)
+        {
+            if (intervals == null || intervals.Length == 0)
+                return new int[][] { newInterval };
+
+            List<int[]> res = new List<int[]>();
+
+            // 遍历区间列表：
+            // 首先将新区间左边且相离的区间加入结果集
+            int i = 0;
+            while (i < intervals.Length && intervals[i][1] < newInterval[0])
+            {
+                res.Add(intervals[i++]);
+            }
+
+            // 接着判断当前区间是否与新区间重叠，重叠的话就进行合并，直到遍历到当前区间在新区间的右边且相离，
+            while (i < intervals.Length && intervals[i][0] <= newInterval[1])
+            {
+                newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
+                newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
+                i++;
+            }
+            // 将最终合并后的新区间加入结果集
+            res.Add(newInterval);
+            
+            // 最后将新区间右边且相离的区间加入结果集
+            while (i < intervals.Length)
+            {
+                res.Add(intervals[i++]);
+            }
+
+            return res.ToArray();
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/insert-interval-test.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class insert_interval_test
+    {
+        insert_interval target = new insert_interval();
+
+        [Test]
+        public void test()
+        {
+            //合并区间
+            int[][] nums = new[]
+            {
+                new[] {1, 3},
+                new[] {6, 9},
+            };
+            int[] interval = new[] { 2, 5 };
+            var res = target.Insert(nums, interval);
+            Assert.IsTrue(ArrayToString(res)=="1,5,6,9");
+
+
+            nums = new[]
+            {
+                new[] {1,2},
+                new[] {3,5},
+                new[] {6,7},
+                new[] {8,10},
+                new[] {12,16},
+            };
+            interval = new[] { 4,8 };
+            res = target.Insert(nums, interval);
+            Assert.IsTrue(ArrayToString(res)=="1,2,3,10,12,16");
+            
+            //新增区间
+            nums = null;
+            interval = new[] { 4, 8 };
+            res = target.Insert(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "4,8");
+
+            //新增区间在头部
+            nums = new[]
+            {
+                new[] {6, 9},
+            };
+            interval = new[] { 1, 3 };
+            res = target.Insert(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "1,3,6,9");
+
+            //新增区间在尾部
+            nums = new[]
+            {
+                new[] { 1, 3},
+            };
+            interval = new[] { 6, 9 };
+            res = target.Insert(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "1,3,6,9");
+        }
+
+        [Test]
+        public void test_V2()
+        {
+            //合并区间
+            int[][] nums = new[]
+            {
+                new[] {1, 3},
+                new[] {6, 9},
+            };
+            int[] interval = new[] { 2, 5 };
+            var res = target.Insert_V2(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "1,5,6,9");
+
+
+            nums = new[]
+            {
+                new[] {1,2},
+                new[] {3,5},
+                new[] {6,7},
+                new[] {8,10},
+                new[] {12,16},
+            };
+            interval = new[] { 4, 8 };
+            res = target.Insert_V2(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "1,2,3,10,12,16");
+
+            //新增区间
+            nums = null;
+            interval = new[] { 4, 8 };
+            res = target.Insert_V2(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "4,8");
+
+            //新增区间在头部
+            nums = new[]
+            {
+                new[] {6, 9},
+            };
+            interval = new[] { 1, 3 };
+            res = target.Insert_V2(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "1,3,6,9");
+
+            //新增区间在尾部
+            nums = new[]
+            {
+                new[] { 1, 3},
+            };
+            interval = new[] { 6, 9 };
+            res = target.Insert_V2(nums, interval);
+            Assert.IsTrue(ArrayToString(res) == "1,3,6,9");
+        }
+
+
+
+
+
+        private string ArrayToString(int[][] input)
+        {
+            if(input==null || input.Length==0) return String.Empty;
+
+            return string.Join(",", input.SelectMany(i => i));
+        }
+    }
+}
