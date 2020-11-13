45a430ab3a42fbaa4350563ad0de15a71e5a455e
ManagerSi <smallvv@126.com>
2020-11-13 21:54:29 +0800

    328. 奇偶链表

---
+++ Sort/Leetcode/Problems/odd-even-linked-list.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 328. 奇偶链表
+    /// https://leetcode-cn.com/problems/odd-even-linked-list/
+    /// </summary>
+    public class odd_even_linked_list
+    {
+        /// <summary>
+        /// 双指针
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode OddEvenList(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            ListNode evenHead = head.next;
+            ListNode odd = head;//奇数
+            ListNode even = evenHead;//偶数
+            while (even?.next != null)
+            {
+                //1,2,3,4
+                odd.next = even.next;
+                odd = odd.next;
+
+                even.next = odd.next;
+                even = even.next;
+            }
+
+            odd.next = evenHead;
+            return head;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/odd-even-linked-list-test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class odd_even_linked_list_test
+    {
+        odd_even_linked_list target = new odd_even_linked_list();
+
+        [Test]
+        public void test()
+        {
+            ListNode node = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5)))));
+            var res = target.OddEvenList(node);
+            Assert.IsTrue(res.ToString() == "1,3,5,2,4");
+
+            node = new ListNode(2, new ListNode(1, new ListNode(3, new ListNode(5, new ListNode(6,new ListNode(4,new ListNode(7)))))));
+            res = target.OddEvenList(node);
+            Assert.IsTrue(res.ToString() == "2,3,6,7,1,5,4");
+        }
+    }
+}
