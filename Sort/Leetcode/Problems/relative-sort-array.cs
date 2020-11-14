bfe3c705a36c32a2553753d9c17ce235d91794f1
ManagerSi <smallvv@126.com>
2020-11-14 21:38:17 +0800

    1122. 数组的相对排序

---
+++ Sort/Leetcode/Problems/relative-sort-array.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 1122. 数组的相对排序
+    /// https://leetcode-cn.com/problems/relative-sort-array/
+    /// </summary>
+    public class relative_sort_array
+    {
+        public int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            if (arr1 == null || arr1.Length <= 1) return arr1;
+
+            int a1Index = 0;
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = a1Index; j < arr1.Length; j++)
+                {
+                    if (arr1[j] == arr2[i])
+                    {
+                        swap(arr1, j, a1Index);
+                        a1Index++;
+                    }
+                }
+            }
+
+            Array.Sort(arr1, a1Index, arr1.Length - a1Index);
+
+            return arr1;
+        }
+
+        private void swap(int[] arr1, int j, int a1Index)
+        {
+            if (j == a1Index) return;
+            if (j < 0 || j >= arr1.Length || a1Index < 0 || a1Index >= arr1.Length) return;
+
+            int temp = arr1[j];
+            arr1[j] = arr1[a1Index];
+            arr1[a1Index] = temp;
+        }
+
+
+        public int[] RelativeSortArray_V2(int[] arr1, int[] arr2)
+        {
+            Dictionary<int, int> num2index = Enumerable.Range(0, arr2.Length)
+                .ToDictionary(p => arr2[p], p => p);
+            return arr1.Where(p => num2index.ContainsKey(p))
+                .OrderBy(p => num2index[p])
+                .Concat(arr1.Where(p => !num2index.ContainsKey(p)).OrderBy(p => p))
+                .ToArray();
+        }
+        public int[] RelativeSortArray_V3(int[] arr1, int[] arr2)
+        {
+            List<int> list = arr2.ToList();
+            arr1 = arr1.OrderBy(a => list.IndexOf(a)).OrderBy(a => list.IndexOf(a) == -1 ? a : 0).ToArray();
+            return arr1;
+        }
+
+    }
+}
---
+++ Sort/LeetcodeTests/relative-sort-array-test.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class relative_sort_array_test
+    {
+        relative_sort_array target =new relative_sort_array();
+
+        [Test]
+        public void test()
+        {
+            var a1 = new int[] { 5,3,1 };
+            var a2 = new int[] {  };
+            var res = target.RelativeSortArray(a1, a2);
+            Assert.True(res.ToArrayString() == "1,3,5");
+
+            a1 = new int[] { 2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19 };
+            a2 = new int[] { 2, 1, 4, 3, 9, 6 };
+            res = target.RelativeSortArray(a1, a2);
+            Assert.True(res.ToArrayString() == "2,2,2,1,4,3,3,9,6,7,19");
+
+        }
+
+        [Test]
+        public void test_V2()
+        {
+            var a1 = new int[] { 5, 3, 1 };
+            var a2 = new int[] { };
+            var res = target.RelativeSortArray_V2(a1, a2);
+            Assert.True(res.ToArrayString() == "1,3,5");
+
+            a1 = new int[] { 2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19 };
+            a2 = new int[] { 2, 1, 4, 3, 9, 6 };
+            res = target.RelativeSortArray_V2(a1, a2);
+            Assert.True(res.ToArrayString() == "2,2,2,1,4,3,3,9,6,7,19");
+        }
+        [Test]
+        public void test_V3()
+        {
+            var a1 = new int[] { 5, 3, 1 };
+            var a2 = new int[] { };
+            var res = target.RelativeSortArray_V3(a1, a2);
+            Assert.True(res.ToArrayString() == "1,3,5");
+
+            a1 = new int[] { 2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19 };
+            a2 = new int[] { 2, 1, 4, 3, 9, 6 };
+            res = target.RelativeSortArray_V3(a1, a2);
+            Assert.True(res.ToArrayString() == "2,2,2,1,4,3,3,9,6,7,19");
+        }
+    }
+}
