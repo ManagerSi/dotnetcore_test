174b6420265487e9af52248ed4dbae0d565c1e7c
ManagerSi <smallvv@126.com>
2020-11-24 21:16:36 +0800

    222. 完全二叉树的节点个数

---
+++ Sort/Leetcode/Problems/count-complete-tree-nodes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 222. 完全二叉树的节点个数
+    /// https://leetcode-cn.com/problems/count-complete-tree-nodes/
+    /// </summary>
+    public class count_complete_tree_nodes
+    {
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            return CountNodes(root.left) + CountNodes(root.right) + 1;
+        }
+
+        public int CountNodes_v2(TreeNode root) 
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int left = countLevel(root.left);
+            int right = countLevel(root.right);
+            if (left == right)
+            {
+                return CountNodes_v2(root.right) + (1 << left);
+            }
+            else
+            {
+                return CountNodes_v2(root.left) + (1 << right);
+            }
+        }
+        private int countLevel(TreeNode root)
+        {
+            int level = 0;
+            while (root != null)
+            {
+                level++;
+                root = root.left;
+            }
+            return level;
+        }
+
+    }
+}
---
+++ Sort/LeetcodeTests/count-complete-tree-nodes-test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class count_complete_tree_nodes_test
+    {
+        count_complete_tree_nodes target = new count_complete_tree_nodes();
+
+        static IEnumerable<TestCaseData> geTestCaseDatas()
+        {
+
+            yield return new TestCaseData(null).Returns(0);
+            yield return new TestCaseData(new TreeNode(1)).Returns(1);
+            yield return new TestCaseData(new TreeNode(1,new TreeNode(2,new TreeNode(4),new TreeNode(5)),new TreeNode(3,new TreeNode(6)))).Returns(6);
+        }
+
+        [Test]
+       [TestCaseSource("geTestCaseDatas")]
+        public int test(TreeNode n)
+        {
+            return target.CountNodes(n);
+        }
+
+
+        [Test]
+        [TestCaseSource("geTestCaseDatas")]
+        public int test_V2(TreeNode n)
+        {
+            return target.CountNodes_v2(n);
+        }
+    }
+}
