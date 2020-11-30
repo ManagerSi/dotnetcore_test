8312d4b99cec138489bcdfc18f77e2d1d8b5074f
ManagerSi <smallvv@126.com>
2020-11-30 21:48:48 +0800

    767. 重构字符串

---
+++ Sort/Leetcode/Problems/reorganize-string.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 767. 重构字符串
+    /// https://leetcode-cn.com/problems/reorganize-string/
+    /// </summary>
+    public class reorganize_string
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="S"></param>
+        /// <returns></returns>
+        public string ReorganizeString(string S)
+        {
+            if (string.IsNullOrWhiteSpace(S)) return string.Empty;
+            
+            //统计个数
+            Dictionary<char,int> dic = new Dictionary<char, int>();
+            int maxCout = 0;
+            int maxChar;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (dic.ContainsKey(S[i]))
+                    dic[S[i]] += 1;
+                else
+                    dic.Add(S[i],1);
+
+                if (dic[S[i]] > maxCout)
+                {
+                    maxCout = dic[S[i]];
+                    maxChar = S[i];
+                }
+            }
+
+            if (S.Length %2 ==0 && maxCout > S.Length / 2 //偶数
+                || S.Length % 2 == 1 && maxCout > S.Length / 2 + 1)  //奇数
+                return string.Empty;
+
+            int ind = 0;
+            char[] res = new char[S.Length];
+            foreach (var keyValuePair in dic.OrderByDescending(i=>i.Value))
+            {
+                int count = keyValuePair.Value;
+                while (count != 0)
+                {
+                    res[ind] = keyValuePair.Key;
+                    ind = ind + 2;
+                    if (ind >= S.Length)
+                        ind = 1;
+
+                    count -= 1;
+                }
+            }
+
+            return string.Join("", res);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/reorganize-string-test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class reorganize_string_test
+    {
+        reorganize_string target =new reorganize_string();
+
+        [Test]
+        [TestCase("abbabbaaab", ExpectedResult = "ababababab")]
+        [TestCase("vvvlo", ExpectedResult = "vlvov")]
+        [TestCase("aab", ExpectedResult = "aba")]
+        [TestCase("a", ExpectedResult = "a")]
+        public string test(string s)
+        {
+            return target.ReorganizeString(s);
+        }
+    }
+}
