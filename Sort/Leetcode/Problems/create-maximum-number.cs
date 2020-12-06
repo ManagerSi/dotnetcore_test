d932e1022346a844dc3acb494b511c9e58d6424e
ManagerSi <smallvv@126.com>
2020-12-06 21:48:49 +0800

    321. 拼接最大数

---
+++ Sort/Leetcode/Problems/create-maximum-number.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Leetcode.Extend;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 321. 拼接最大数
+    /// https://leetcode-cn.com/problems/create-maximum-number/
+    /// </summary>
+    public class create_maximum_number
+    {
+        public int[] MaxNumber(int[] nums1, int[] nums2, int k)
+        {
+            int[] maxRes = new int[0];
+            for (int i = 0; i < k+1; i++)
+            {
+                int[] n1Max = PickMax(nums1, i);
+                int[] n2Max = PickMax(nums2, k-i);
+                int[] res = Merge(n1Max, n2Max);
+
+                if (res.Length > maxRes.Length 
+                    || (res.Length == maxRes.Length && Compare(res, 0, maxRes, 0)))
+                    maxRes = res;
+            }
+
+            return maxRes;
+        }
+
+        private int[] PickMax(int[] num, int k)
+        {
+            if (num==null || k <= 0)
+                return new int[] { };
+
+            if (k >= num.Length)
+                return num;
+
+            int drop = num.Length - k;
+            Stack<int> stack = new Stack<int>();
+            foreach (var n in num)
+            {
+                while (stack.Count > 0 && n > stack.Peek() && drop > 0)
+                {
+                    stack.Pop();
+                    drop--;
+                }
+
+                stack.Push(n);
+            }
+
+            for (int i = 0; i < drop; i++)
+            {
+                stack.Pop();
+            }
+
+            int[] res = new int[stack.Count];
+            int index = res.Length - 1;
+            while (stack.Count > 0)
+            {
+                res[index--] = stack.Pop();
+            }
+
+            return res;
+        }
+
+        private int[] Merge(int[] nums1, int[] nums2)
+        {
+            int x = nums1.Length, y = nums2.Length;
+            if (x == 0)
+            {
+                return nums2;
+            }
+            if (y == 0)
+            {
+                return nums1;
+            }
+            
+            int[] res = new int[x + y];
+            int p1 = 0;
+            int p2 = 0;
+            // 这里原来两个数组未必有序，为了让大的元素尽可能往前放，比较的时候若当前元素相等还要往后比较
+            for (int i = 0; i < res.Length; i++)
+            {
+                if (Compare(nums1, p1, nums2, p2))
+                {
+                    res[i] = nums1[p1++];
+                }
+                else
+                {
+                    res[i] = nums2[p2++];
+                }
+            }
+            return res;
+        }
+
+        // 相当于把数组组成的数字左对齐后比较大小
+        private bool Compare(int[] nums1, int p1, int[] nums2, int p2)
+        {
+            if (p2 >= nums2.Length) return true;
+            if (p1 >= nums1.Length) return false;
+            if (nums1[p1] > nums2[p2]) return true;
+            if (nums1[p1] < nums2[p2]) return false;
+            return Compare(nums1, p1 + 1, nums2, p2 + 1);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/create_maximum_number-test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class create_maximum_number_test
+    {
+        create_maximum_number target = new create_maximum_number();
+        [TestCase(new int[]{ 3, 4, 6, 5 }, new int[] { 9, 1, 2, 5, 8, 3 }, 5, ExpectedResult = new int[] { 9, 8, 6, 5, 3 })]
+        [TestCase(new int[] { 6, 7 }, new int[] { 6, 0, 4 }, 5, ExpectedResult = new int[] { 6, 7, 6, 0, 4 })]
+        [TestCase(new int[] { 3, 9 }, new int[] { 8, 9 }, 3, ExpectedResult = new int[] { 9, 8, 9 })]
+        [TestCase(new int[] { }, new int[] { 8, 9 }, 1, ExpectedResult = new int[] { 9 })]
+        [TestCase(new int[] { }, new int[] { 8, 9 }, 0, ExpectedResult = new int[] {  })]
+        [TestCase(
+            new int[] { 4, 6, 9, 1, 0, 6, 3, 1, 5, 2, 8, 3, 8, 8, 4, 7, 2, 0, 7, 1, 9, 9, 0, 1, 5, 9, 3, 9, 3, 9, 7, 3, 0, 8, 1, 0, 9, 1, 6, 8, 8, 4, 4, 5, 7, 5, 2, 8, 2, 7, 7, 7, 4, 8, 5, 0, 9, 6, 9, 2 }, 
+            new int[] { 9, 9, 4, 5, 1, 2, 0, 9, 3, 4, 6, 3, 0, 9, 2, 8, 8, 2, 4, 8, 6, 5, 4, 4, 2, 9, 5, 0, 7, 3, 7, 5, 9, 6, 6, 8, 8, 0, 2, 4, 2, 2, 1, 6, 6, 5, 3, 6, 2, 9, 6, 4, 5, 9, 7, 8, 0, 7, 2, 3 }, 
+            60, 
+            ExpectedResult = new int[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 6, 8, 8, 4, 4, 5, 7, 5, 2, 8, 2, 7, 7, 7, 4, 8, 5, 0, 9, 6, 9, 2, 0, 2, 4, 2, 2, 1, 6, 6, 5, 3, 6, 2, 9, 6, 4, 5, 9, 7, 8, 0, 7, 2, 3 })]
+        public int[] test(int[] a, int[] b,int k)
+        {
+            return target.MaxNumber(a, b, k);
+        }
+    }
+}
