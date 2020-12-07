293c40833de8364df6babf59d6c1cbfbb3533a47
jerry.si <403563310@qq.com>
2020-12-07 23:39:12 +0800

    297. 二叉树的序列化与反序列化

---
+++ Sort/LeetcodeTests/serialize-and-deserialize-binary-tree-test.cs
@@ -0,0 +1,25 @@
+﻿using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    class serialize_and_deserialize_binary_tree_test
+    {
+        serialize_and_deserialize_binary_tree target = new serialize_and_deserialize_binary_tree();
+        [Test]
+        public void test()
+        {
+            TreeNode tree = new TreeNode(1,
+                    new TreeNode(2, new TreeNode(4)),
+                    new TreeNode(3, null, new TreeNode(5)));
+            var str = target.serialize(tree);
+            var node = target.deserialize(str);
+            Assert.IsNotNull(str);
+            Assert.IsNotNull(node);
+        }
+    }
+}
