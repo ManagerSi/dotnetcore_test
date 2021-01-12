d5e4759371657afb353d29ca14e498c498b383d4
ManagerSi <smallvv@126.com>
2021-01-12 21:50:41 +0800

    547. 省份数量

---
+++ Sort/Leetcode/Problems/number-of-provinces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 547. 省份数量
+    /// </summary>
+    public class number_of_provinces
+    {
+        public int FindCircleNum(int[][] isConnected)
+        {
+            int provinces = isConnected.Length;
+            bool[] visited = new bool[provinces];
+            int circles = 0;
+            for (int i = 0; i < provinces; i++)
+            {
+                if (!visited[i])
+                {
+                    dfs(isConnected, visited, provinces, i);
+                    circles++;
+                }
+            }
+            return circles;
+        }
+
+        public void dfs(int[][] isConnected, bool[] visited, int provinces, int i)
+        {
+            for (int j = 0; j < provinces; j++)
+            {
+                if (isConnected[i][j] == 1 && !visited[j])
+                {
+                    visited[j] = true;
+                    dfs(isConnected, visited, provinces, j);
+                }
+            }
+
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/number-of-provinces-test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class number_of_provinces_test
+    {
+        number_of_provinces target = new number_of_provinces();
+
+        static IEnumerable<TestCaseData> getDatas()
+        {
+            yield return new TestCaseData((object)new int[][]
+            {
+                new int[] { 1,1,0 },
+                new int[] { 1,1,0 },
+                new int[] { 0,0,1 },
+            }).Returns(2);
+            yield return new TestCaseData((object)new int[][]
+            {
+                new int[] { 1,0,0 },
+                new int[] { 0,1,0 },
+                new int[] { 0,0,1 },
+            }).Returns(3);
+        }
+        [Test]
+        [TestCaseSource("getDatas")]
+        public int test(int[][] isConnected)
+        {
+            return target.FindCircleNum(isConnected);
+        }
+    }
+}
