21c6f8e3b13a410b9b46b7135911fb50ea227eb9
jerry.si <403563310@qq.com>
2021-01-17 22:45:22 +0800

    1232. 缀点成线 向量法避免单独处理除0的情况

---
+++ Sort/Leetcode/Problems/check-if-it-is-a-straight-line.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 1232. 缀点成线
+    /// </summary>
+    public class check_if_it_is_a_straight_line
+    {
+        /// <summary>
+        /// 向量平行法
+        /// </summary>
+        /// <param name="coordinates"></param>
+        /// <returns></returns>
+        public bool CheckStraightLine(int[][] coordinates)
+        {
+            if (coordinates == null || coordinates.Length < 2) return false;
+
+            for (int i = 2; i < coordinates.Length; i++)
+                if ((coordinates[1][1] - coordinates[0][1]) * (coordinates[i][0] - coordinates[0][0]) != (coordinates[i][1] - coordinates[0][1]) * (coordinates[1][0] - coordinates[0][0]))
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/check-if-it-is-a-straight-line-test.cs
@@ -0,0 +1,31 @@
+﻿using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    class check_if_it_is_a_straight_line_test
+    {
+        check_if_it_is_a_straight_line target = new check_if_it_is_a_straight_line();
+
+        public static IEnumerable<TestCaseData> GetTestCaseDatas()
+        {
+            yield return new TestCaseData((object)new int[][] { }).Returns(false);
+            yield return new TestCaseData((object)new int[][] {
+                new int[] {1, 1 },new int[] {2, 2},new int[] {3, 3},new int[] {4, 4},new int[] {5,5},new int[] {7, 7}
+            }).Returns(true);
+            yield return new TestCaseData((object)new int[][] { 
+                new int[] {1, 1 },new int[] {2, 2},new int[] {3, 4},new int[] {4, 5},new int[] {5, 6},new int[] {7, 7}
+            }).Returns(false);
+        }
+
+        [Test]
+        [TestCaseSource("GetTestCaseDatas")]
+        public bool test(int[][] coordinates)
+        {
+            return target.CheckStraightLine(coordinates);
+        }
+    }
+}
