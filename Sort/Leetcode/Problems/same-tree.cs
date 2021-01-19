257683c79e49f897cb1ac4b147e870f4ceddbba7
jerry.si <403563310@qq.com>
2021-01-19 22:36:58 +0800

    100. 相同的树

---
+++ Sort/Leetcode/Problems/same-tree.cs
@@ -0,0 +1,76 @@
+﻿using Leetcode.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 100. 相同的树
+    /// https://leetcode-cn.com/problems/same-tree/
+    /// </summary>
+    public class same_tree
+    {
+        /// <summary>
+        /// 深度优先搜索
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+
+            if(p?.val == q?.val)
+            {
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 广度优先遍历
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public bool IsSameTree_V2(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+
+            if (p == null || q == null) return false;
+
+            Queue<TreeNode> Q1 = new Queue<TreeNode>();
+            Queue<TreeNode> Q2 = new Queue<TreeNode>();
+            Q1.Enqueue(p);
+            Q2.Enqueue(q);
+            while (Q1.Count>0 && Q2.Count>0)
+            {
+                var node1 = Q1.Dequeue();
+                var node2 = Q2.Dequeue();
+
+                if (node1.val != node2.val) return false;
+
+                if ((node1.left == null) ^ (node2.left == null))
+                    return false;
+
+                if ((node1.right == null) ^ (node2.right == null))
+                    return false;
+
+                if(node1.left!=null)//same as node2
+                { 
+                    Q1.Enqueue(node1.left);
+                    Q2.Enqueue(node2.left);
+                }
+                if (node1.right != null)//same as node2
+                {
+                    Q1.Enqueue(node1.right);
+                    Q2.Enqueue(node2.right);
+                }
+            }
+
+            return Q1.Count == Q2.Count;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/same-tree-test.cs
@@ -0,0 +1,45 @@
+﻿using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    class same_tree_test
+    {
+        same_tree target = new same_tree();
+
+        public static IEnumerable<TestCaseData> Cases()
+        {
+            yield return new TestCaseData(new TreeNode(),new TreeNode()).Returns(true);
+
+            yield return new TestCaseData(
+                new TreeNode(1, new TreeNode(2), new TreeNode(3)), 
+                new TreeNode(1, new TreeNode(2), new TreeNode(3))).Returns(true);
+
+            yield return new TestCaseData(
+                new TreeNode(1, new TreeNode(2), null),
+                new TreeNode(1, null, new TreeNode(2))).Returns(false);
+
+            yield return new TestCaseData(
+                new TreeNode(1, new TreeNode(1), new TreeNode(2)),
+                new TreeNode(1, new TreeNode(2), new TreeNode(1))).Returns(false);
+
+        }
+
+        [Test]
+        [TestCaseSource("Cases")]
+        public bool test(TreeNode p, TreeNode q)
+        {
+            return target.IsSameTree(p, q);
+        }
+
+        [TestCaseSource("Cases")]
+        public bool test_V2(TreeNode p, TreeNode q)
+        {
+            return target.IsSameTree_V2(p, q);
+        }
+    }
+}
