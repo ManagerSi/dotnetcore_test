9d8b10e340813634dc6b94b89977fbbe9a44c177
ManagerSi <smallvv@126.com>
2021-01-20 21:26:24 +0800

    628. 三个数的最大乘积

---
+++ Sort/Leetcode/Problems/maximum-product-of-three-numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 628. 三个数的最大乘积
+    /// </summary>
+    public class maximum_product_of_three_numbers
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            int index = nums.Length - 1;
+            return Math.Max(nums[index] * nums[index - 1] * nums[index - 2], nums[0] * nums[1] * nums[index]);
+        }
+    }
+}
