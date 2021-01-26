fd074be2224b00f35b3f22819425fbd773fcb773
ManagerSi <smallvv@126.com>
2021-01-26 22:02:03 +0800

    1128. 等价多米诺骨牌对的数量

---
+++ Sort/Leetcode/Problems/number-of-equivalent-domino-pairs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 1128. 等价多米诺骨牌对的数量
+    /// https://leetcode-cn.com/problems/number-of-equivalent-domino-pairs/
+    /// </summary>
+    public class number_of_equivalent_domino_pairs
+    {
+        /// <summary>
+        /// 转为字符串
+        /// </summary>
+        /// <param name="dominoes"></param>
+        /// <returns></returns>
+        public int NumEquivDominoPairs(int[][] dominoes)
+        {
+            if (dominoes == null || dominoes.Length == 0) return 0;
+
+            List<string> strList = new List<string>(dominoes.Length);
+            foreach (var domino in dominoes)
+            {
+                strList.Add(domino[0] > domino[1] ? $"{domino[1]},{domino[0]}" : $"{domino[0]},{domino[1]}");
+            }
+            //2个一样->return 1, (n-1)+(n-2)+...+2+1 => n*(n-1)/2
+            //3个一样->return 3,
+            //4个一样->return 6,
+            //5个一样->return 10,
+            return strList.GroupBy(i => i).Sum(i => i.Count() * (i.Count() - 1) / 2);
+
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/number-of-equivalent-domino-pairs-test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class number_of_equivalent_domino_pairs_test
+    {
+        number_of_equivalent_domino_pairs target =new number_of_equivalent_domino_pairs();
+
+        static IEnumerable<TestCaseData> geTestCaseDatas()
+        {
+            yield return new TestCaseData((object)new int[][]
+            {
+                new int[]{1,2},new int[]{2,1},new int[]{3,4},new int[]{5,6},
+            }).Returns(1);
+            yield return new TestCaseData((object)new int[][]
+            {
+                new int[]{1,2},new int[]{2,1},new int[]{2,1},new int[]{3,4},new int[]{5,6},
+            }).Returns(3);
+            yield return new TestCaseData((object)new int[][]
+            {
+                new int[]{1,2},new int[]{2,1},new int[]{2,1},new int[]{2,1},new int[]{3,4},new int[]{5,6},
+            }).Returns(6);
+            yield return new TestCaseData((object)new int[][]
+            {
+                new int[]{1,2},new int[]{2,1},new int[]{2,1},new int[]{2,1},new int[]{2,1},new int[]{3,4},new int[]{5,6},
+            }).Returns(10);
+        }
+
+        [TestCaseSource("geTestCaseDatas")]
+        public int test(int[][] dominoes)
+        {
+            return target.NumEquivDominoPairs(dominoes);
+        }
+    }
+}
