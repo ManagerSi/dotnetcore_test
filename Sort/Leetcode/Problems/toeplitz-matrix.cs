99cd2f57a892d0459f8bcf9dc016b4b871f6e85e
ManagerSi <smallvv@126.com>
2021-02-22 22:10:28 +0800

    766. 托普利茨矩阵

---
+++ Sort/Leetcode/Problems/toeplitz-matrix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 766. 托普利茨矩阵
+    /// https://leetcode-cn.com/problems/toeplitz-matrix/
+    /// </summary>
+    public class toeplitz_matrix
+    {
+        public bool IsToeplitzMatrix(int[][] matrix)
+        {
+            var rows = matrix.Length;
+            var cols = matrix[0].Length;
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                for (int j = 0; j < cols-1; j++)
+                {
+                    if (matrix[i][j] != matrix[i + 1][j + 1])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 按对角线读取
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public bool IsToeplitzMatrix_V2(int[][] matrix)
+        {
+            int m = matrix.Length, n = matrix[0].Length;
+            int row = m, col = n;
+            while (col-- > 0)
+            {
+                for (int i = 0, j = col, val = matrix[i++][j++]; i < m && j < n; i++, j++)
+                {
+                    if (matrix[i][j] != val) return false;
+                }
+            }
+            while (row-- > 0)
+            {
+                for (int i = row, j = 0, val = matrix[i++][j++]; i < m && j < n; i++, j++)
+                {
+                    if (matrix[i][j] != val) return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Sort/LeetcodeTests/toeplitz-matrix-test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class toeplitz_matrix_test
+    {
+        toeplitz_matrix target = new toeplitz_matrix();
+
+        public static IEnumerable<TestCaseData> getTestData()
+        {
+            yield return new TestCaseData(
+                (object)new int[][]
+                {
+                    new []{1,3},
+                    new []{3,1}
+                }).Returns(true);
+
+            yield return new TestCaseData(
+                (object) new int[][]
+                {
+                    new []{1,2,3,4},
+                    new []{5,1,2,3},
+                    new []{9,5,1,2},
+                }).Returns(true);
+            yield return new TestCaseData(
+                (object)new int[][]
+                {
+                    new []{1,3},
+                    new []{3,2}
+                }).Returns(false);
+            yield return new TestCaseData(
+                (object)new int[][]
+                {
+                    new []{1,2},
+                    new []{2,1},
+                    new []{3,2},
+                    new []{4,3},
+                    new []{5,4},
+                }).Returns(true);
+        }
+
+        [TestCaseSource("getTestData")]
+        public bool test(int[][] nums)
+        {
+            return target.IsToeplitzMatrix(nums);
+        }
+
+        [TestCaseSource("getTestData")]
+        public bool test_V2(int[][] nums)
+        {
+            return target.IsToeplitzMatrix_V2(nums);
+        }
+    }
+}
