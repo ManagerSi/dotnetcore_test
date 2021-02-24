212179ca5ba0a2e082c4dc4145340c655af428ba
ManagerSi <smallvv@126.com>
2021-02-24 21:58:48 +0800

    832. 翻转图像

---
+++ Sort/Leetcode/Problems/flipping-an-image.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 832. 翻转图像
+    /// https://leetcode-cn.com/problems/flipping-an-image/
+    /// </summary>
+    public class flipping_an_image
+    {
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            int[][] result = new int[A.Length][];
+            for (int i = 0; i < A.Length; i++)
+            {
+                var length = A[i].Length;
+                result[i] = new int[length];
+                for (int j = length - 1; j >= 0; j--)
+                {
+                    result[i][length - j - 1] = A[i][j] == 0 ? 1 : 0;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/flipping-an-image-test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class flipping_an_image_test
+    {
+        flipping_an_image target = new flipping_an_image();
+
+        public static IEnumerable<TestCaseData> GetTestCaseDatas()
+        {
+            yield return new TestCaseData(
+                    (object)new int[][]
+                    {
+                        new[]{ 1, 1, 0 },
+                        new[] { 1, 0, 1 },
+                        new[] { 0, 0, 0 },
+                    })
+                    .Returns((object)new int[][]
+                    {
+                        new[]{  1,0,0 },
+                        new[] {0,1,0 },
+                        new[] { 1,1,1 },
+                    });
+
+            yield return new TestCaseData(
+                    (object)new int[][]
+                    {
+                        new[]{ 1,1,0,0 },
+                        new[] { 1,0,0,1 },
+                        new[] {0,1,1,1 },
+                        new[] {1,0,1,0 },
+                    })
+                .Returns((object)new int[][]
+                {
+                    new[]{ 1,1,0,0 },
+                    new[] {0,1,1,0 },
+                    new[] { 0,0,0,1 },
+                    new[] { 1,0,1,0 },
+                });
+    }
+
+        [Test]
+        [TestCaseSource("GetTestCaseDatas")]
+        public int[][] test(int[][] coordinates)
+        {
+            return target.FlipAndInvertImage(coordinates);
+        }
+    }
+}
