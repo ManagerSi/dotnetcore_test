f5859bec051796a6bd07f46bf24af6d3bf7fbc6d
ManagerSi <smallvv@126.com>
2021-03-01 22:48:53 +0800

    303. 区域和检索 - 数组不可变

---
+++ Sort/Leetcode/Problems/range-sum-query-immutable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 303. 区域和检索 - 数组不可变
+    /// </summary>
+    public class range_sum_query_immutable
+    {
+        /// <summary>
+        /// 暴力解题
+        /// </summary>
+        public class NumArray
+        {
+            public NumArray(int[] nums)
+            {
+                _nums = new int[nums.Length];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    _nums[i] = nums[i];
+                }
+            }
+
+            private int[] _nums;
+            public int SumRange(int i, int j)
+            {
+                int sum = 0;
+                for (int k = i; k <= j; k++)
+                {
+                    sum += _nums[k];
+                }
+
+                return sum;
+            }
+        }
+
+        /// <summary>
+        /// 前缀和
+        /// </summary>
+        public class NumArray_V2
+        {
+            public NumArray_V2(int[] nums)
+            {
+                _sums = new int[nums.Length+1];
+                _sums[0] = 0;
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    _sums[i+1] = _sums[i] + nums[i];
+                }
+            }
+
+            private int[] _sums;
+            public int SumRange(int i, int j)
+            {
+               return _sums[j+1] - _sums[i];
+            }
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/range-sum-query-immutable-test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class range_sum_query_immutable_test
+    {
+        
+        [Test]
+        public void test()
+        {
+            int[] nums = new[] {-2, 0, 3, -5, 2, -1};
+            range_sum_query_immutable.NumArray target = new range_sum_query_immutable.NumArray(nums);
+            int result = 0;
+
+            result = target.SumRange(0, 2);
+            Assert.True(result == 1);
+
+            result = target.SumRange(2, 5);
+            Assert.True(result == -1);
+
+            result = target.SumRange(0, 5);
+            Assert.True(result == -3);
+
+        }
+
+        [Test]
+        public void test_V2()
+        {
+            int[] nums = new[] { -2, 0, 3, -5, 2, -1 };
+            range_sum_query_immutable.NumArray_V2 target = new range_sum_query_immutable.NumArray_V2(nums);
+            int result = 0;
+
+            result = target.SumRange(0, 2);
+            Assert.True(result == 1);
+
+            result = target.SumRange(2, 5);
+            Assert.True(result == -1);
+
+            result = target.SumRange(0, 5);
+            Assert.True(result == -3);
+
+        }
+    }
+}
