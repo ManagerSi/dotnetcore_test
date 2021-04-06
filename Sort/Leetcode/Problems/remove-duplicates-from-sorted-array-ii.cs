00d42d4ca776ec09ec9d600d4d8ce50ecd91aa5f
ManagerSi <smallvv@126.com>
2021-04-07 00:15:32 +0800

    80. 删除有序数组中的重复项 II

---
+++ Sort/Leetcode/Problems/remove-duplicates-from-sorted-array-ii.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 80. 删除有序数组中的重复项 II
+    /// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+    /// </summary>
+    public class remove_duplicates_from_sorted_array_ii
+    {
+        private readonly int _limit = 2;
+        public int RemoveDuplicates(int[] nums)
+        {
+            int pre = int.MinValue;
+            int dupCount = 0;
+            int delCount = 0;
+            int end = nums.Length;
+            for (int i = 0; i < end; i++)
+            {
+                if (nums[i] != pre)
+                {
+                    dupCount=1;
+                    pre = nums[i];
+                    continue;
+                }
+
+                dupCount++;
+                if (dupCount > _limit)
+                {
+                    if (nums[i] == nums[nums.Length - 1])
+                    {
+                        delCount += nums.Length - i - delCount;
+                        break;
+                    }
+
+                    DeleteByIndex(nums, i);
+                    delCount++;
+                    end--;
+                    i--;
+                }
+
+            }
+
+            return nums.Length - delCount;
+        }
+
+        private void DeleteByIndex(int[] nums, int i)
+        {
+            for (int j = i; j < nums.Length-1; j++)
+            {
+                nums[j] = nums[j + 1];
+            }
+        }
+
+        /// <summary>
+        /// 双指针
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int RemoveDuplicates_V2(int[] nums)
+        {
+            int len = nums.Length;
+            if (len < 3)
+            {
+                return len;
+            }
+            int slow = 2, fast = 2;
+            while (fast < len)
+            {
+                //检查上上个应该被保留的元素nums[slow−2] 是否和当前待检查元素nums[fast] 相同
+                if (nums[slow - 2] != nums[fast])
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+                fast++;
+            }
+            return slow;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/remove-duplicates-from-sorted-array-ii-test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class remove_duplicates_from_sorted_array_ii_test
+    {
+        remove_duplicates_from_sorted_array_ii target = new remove_duplicates_from_sorted_array_ii();
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 3 }, ExpectedResult = 4)]
+        [TestCase(new int[] { 1, 2, 3, 3, 3 }, ExpectedResult = 4)]
+        [TestCase(new int[] { 1, 2, 2, 2, 3, 3 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 1, 1, 1, 2 }, ExpectedResult = 3)]
+        [TestCase(new int[] { 1, 1, 1 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 }, ExpectedResult = 7)]
+        [TestCase(new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 3 }, ExpectedResult = 7)]
+        [TestCase(new int[] { 1, 1, 1, 1, 2, 2, 3 }, ExpectedResult = 5)]
+        public int test(int[] nums)
+        {
+            return target.RemoveDuplicates(nums);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 3 }, ExpectedResult = 4)]
+        [TestCase(new int[] { 1, 2, 3, 3, 3 }, ExpectedResult = 4)]
+        [TestCase(new int[] { 1, 2, 2, 2, 3, 3 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 1, 1, 1, 2 }, ExpectedResult = 3)]
+        [TestCase(new int[] { 1, 1, 1 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 }, ExpectedResult = 7)]
+        [TestCase(new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 3 }, ExpectedResult = 7)]
+        [TestCase(new int[] { 1, 1, 1, 1, 2, 2, 3 }, ExpectedResult = 5)]
+        public int test_v2(int[] nums)
+        {
+            return target.RemoveDuplicates_V2(nums);
+        }
+    }
+}
