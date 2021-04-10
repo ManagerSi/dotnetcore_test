3da036ca29c64f1f89523e3d3909f51448e64f94
ManagerSi <smallvv@126.com>
2021-04-10 23:27:25 +0800

    FluentValidation test refer to : https://docs.fluentvalidation.net/en/latest/aspnet.html

---
+++ CoreTest/WebFluentValidationTest/WebFluentValidationTest/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebFluentValidationTest.Model;
+
+namespace WebFluentValidationTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            if (!ModelState.IsValid)
+            { // re-render the view when validation failed.
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+	}
+}
---
+++ CoreTest/WebFluentValidationTest/WebFluentValidationTest/Validator/PersonValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using WebFluentValidationTest.Model;
+
+namespace WebFluentValidationTest.Validator
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Name).Length(0, 10);
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Age).InclusiveBetween(18, 60);
+        }
+    }
+
+}
