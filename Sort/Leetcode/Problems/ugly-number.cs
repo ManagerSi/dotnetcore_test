8ce5e8e145f8dbb8d9a94fe572b726a5f4feb84e
ManagerSi <smallvv@126.com>
2021-04-10 23:02:40 +0800

    263. 丑数

---
+++ Sort/Leetcode/Problems/ugly-number.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 263. 丑数
+    /// https://leetcode-cn.com/problems/ugly-number/
+    /// </summary>
+    public class ugly_number
+    {
+        public bool IsUgly(int n)
+        {
+            if (n <= 0) return false;
+
+            int res = n;
+            while (res != 0)
+            {
+                if (res % 2 == 0) { res = res / 2; continue; }
+                if (res % 3 == 0) { res = res / 3; continue; }
+                if (res % 5 == 0) { res = res / 5; continue; }
+
+                return res == 1;
+            }
+
+            return res == 1;
+        }
+
+        public bool IsUgly_V2(int n)
+        {
+            if (n < 1)
+            {
+                return false;
+            }
+            while (n % 2 == 0)
+            {
+                // n /= 2;
+                // 位运算更省内存
+                n >>= 1;
+            }
+            while (n % 3 == 0)
+            {
+                n /= 3;
+            }
+            while (n % 5 == 0)
+            {
+                n /= 5;
+            }
+
+            return n == 1;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/ugly-number-test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class ugly_number_test
+    {
+        ugly_number target = new ugly_number();
+
+        [Test]
+        [TestCase(1, ExpectedResult = true)]
+        [TestCase(6,ExpectedResult = true)]
+        [TestCase(8, ExpectedResult = true)]
+        [TestCase(14, ExpectedResult = false)]
+        public bool test(int n)
+        {
+            return target.IsUgly(n);
+        }
+
+        [Test]
+        [TestCase(1, ExpectedResult = true)]
+        [TestCase(6, ExpectedResult = true)]
+        [TestCase(8, ExpectedResult = true)]
+        [TestCase(14, ExpectedResult = false)]
+        public bool test_V2(int n)
+        {
+            return target.IsUgly_V2(n);
+        }
+    }
+}
