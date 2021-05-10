8c288b60039873c802f82ce03320ecea8535409b
ManagerSi <smallvv@126.com>
2021-05-10 22:33:01 +0800

    872. 叶子相似的树

---
+++ Sort/Leetcode/Problems/leaf-similar-trees.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 872. 叶子相似的树
+    /// https://leetcode-cn.com/problems/leaf-similar-trees/
+    /// </summary>
+    public class leaf_similar_trees
+    {
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            string list1 = GetLeafs(root1);
+            string list2 = GetLeafs(root2);
+
+            return list1 == list2;
+        }
+
+        private string GetLeafs(TreeNode treeNode)
+        {
+            if (treeNode == null) return null;
+
+            List<int> sb = new List<int>();
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(treeNode);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                if (node.left == null && node.right == null)
+                    sb.Add(node.val);
+
+                if(node.right!=null)
+                    stack.Push(node.right);
+
+                if (node.left != null)
+                    stack.Push(node.left);
+            }
+
+            var res = string.Join(',', sb);
+            Console.WriteLine(res);
+            return res;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/leaf-similar-trees-test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class leaf_similar_trees_test
+    {
+        private leaf_similar_trees target = new leaf_similar_trees();
+
+        static IEnumerable<TestCaseData> geTestCaseDatas()
+        {
+            yield return new TestCaseData(new TreeNode(1), new TreeNode(1)).Returns(true);
+            yield return new TestCaseData(new TreeNode(1), new TreeNode(2)).Returns(false);
+            yield return new TestCaseData(new TreeNode(1,new TreeNode(2)), new TreeNode(2,null,new TreeNode(2))).Returns(true);
+            yield return new TestCaseData(
+                new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+                new TreeNode(1, new TreeNode(3), new TreeNode(2))
+                ).Returns(false);
+            yield return new TestCaseData(
+                new TreeNode(3, new TreeNode(5,new TreeNode(6),new TreeNode(71)), new TreeNode(1,new TreeNode(4,new TreeNode(2,new TreeNode(9),new TreeNode(8))))),
+                new TreeNode(3, new TreeNode(5,new TreeNode(6),new TreeNode(2,new TreeNode(7),new TreeNode(14))), new TreeNode(1,new TreeNode(9),new TreeNode(8)))
+            ).Returns(false);
+
+        }
+
+        [Test]
+        [TestCaseSource("geTestCaseDatas")]
+        public bool test(TreeNode t1, TreeNode t2)
+        {
+            return target.LeafSimilar(t1, t2);
+        }
+    }
+}
