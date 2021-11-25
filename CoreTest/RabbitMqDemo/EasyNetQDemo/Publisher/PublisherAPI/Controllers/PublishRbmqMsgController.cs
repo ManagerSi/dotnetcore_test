87b019aee67e710d92a1ceaa076b892e9e5d0920
ManagerSi <smallvv@126.com>
2021-11-26 01:06:52 +0800

    添加 EasyNetQ 的测试项目，实现简单的网页发送，控制台接收

---
+++ CoreTest/RabbitMqDemo/EasyNetQDemo/Publisher/PublisherAPI/Controllers/PublishRbmqMsgController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyNetQ;
+
+namespace Publisher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PublishRbmqMsgController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return new JsonResult("ok");
+        }
+
+        [HttpGet]
+        [Route("PublishDefaultMsage")]
+        public async Task<IActionResult> PublishDefaultMsage()
+        {
+            string connectionStr = "host=localhost;username=guest;password=guest";
+
+            using (var bus = RabbitHutch.CreateBus(connectionStr))
+            {
+                var input = "abcd";
+                Console.WriteLine("PublishDefaultMsage: PubSub.PublishAsync");
+                await bus.PubSub.PublishAsync(input);
+            }
+            return new JsonResult("ok");
+        }
+
+
+        [HttpGet]
+        [Route("PublishClassMsage")]
+        public async Task<IActionResult> PublishClassMsage()
+        {
+            string connectionStr = "host=localhost;username=guest;password=guest";
+
+            using (var bus = RabbitHutch.CreateBus(connectionStr))
+            {
+                var input = "abcd";
+                Console.WriteLine("PublishDefaultMsage: PubSub.PublishAsync");
+                await bus.PubSub.PublishAsync<TextMessage>(new TextMessage { Text = input});//注意，收发必须是相同的class，包括命名空间，否则接收时报错
+            }
+            return new JsonResult("ok");
+        }
+
+        [Queue("TestMessagesQueue", ExchangeName = "MyTestExchange")]
+        public class TextMessage
+        {
+            public string Text { get; set; }
+        }
+    }
+}
---
+++ CoreTest/RabbitMqDemo/EasyNetQDemo/Subscriber/SubscriberConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using EasyNetQ;
+using System;
+
+namespace SubscriberConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string connectionStr = "host=localhost;username=guest;password=guest";
+
+            using (var bus = RabbitHutch.CreateBus(connectionStr))
+            {
+                bus.PubSub.SubscribeAsync<string>(string.Empty, HandleTextMessage);
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        public static void HandleTextMessage(string textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage);
+            Console.ResetColor();
+        }
+    }
+
+    //注意，收发必须是相同的class，包括命名空间，否则接收时报错
+    [Queue("TestMessagesQueue", ExchangeName = "MyTestExchange")]
+    public class TextMessage
+    {
+        public string Text { get; set; }
+    }
+}
