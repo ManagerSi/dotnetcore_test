363e784ed028392e44f641e79bcdb4a5412dee17
ManagerSi <smallvv@126.com>
2022-03-05 00:04:53 +0800

    添加258. 各位相加

---
+++ Sort/Leetcode/Problems/add-digits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 258. 各位相加
+    /// https://leetcode-cn.com/problems/add-digits/
+    /// </summary>
+    public class add_digits
+    {
+        public int AddDigits(int num)
+        {
+            if (num < 10) return num;
+
+            var temp = num;
+            while (temp >= 10)
+            {
+                num = temp;
+                temp = 0;
+
+                while (num > 0)
+                {
+                    temp += num % 10;
+                    num = num / 10;
+                }
+            }
+
+            return temp;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/add-digits-test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class add_digits_test
+    {
+        private add_digits target = new add_digits();
+
+
+        [TestCase(38,ExpectedResult = 2)]
+        [TestCase(10, ExpectedResult = 1)]
+        public int test(int num)
+        {
+            return target.AddDigits(num);
+        }
+    }
+}
