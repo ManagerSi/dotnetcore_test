e9ae174a9cb0b71a6f160f50dd2ac540ee039ef6
ManagerSi <smallvv@126.com>
2022-03-11 23:36:49 +0800

    add leetcode binary-search

---
+++ Sort/Leetcode/Problems/binary-search.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    public class binary_search
+    {
+        public int Search(int[] nums, int target)
+        {
+
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/binary-search-test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace LeetcodeTests
+{
+    public class binary_search_test
+    {
+        private binary_search _target;
+        
+        [SetUp]
+        public void init()
+        {
+            _target = new binary_search();
+        }
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, }, 0, ExpectedResult = -1)]
+        [TestCase(new int[] { 1, 2, 3, }, 5, ExpectedResult = -1)]
+        [TestCase(new int[] { 1, 2, 3, },3, ExpectedResult = 2)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 },5, ExpectedResult = 4)]
+        public int test(int[] nums, int target)
+        {
+            return _target.Search(nums, target);
+        }
+    }
+}
