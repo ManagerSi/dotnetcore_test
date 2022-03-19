ec0a71cd1a189b288e1cf0ce179f7de747c99f4e
ManagerSi <smallvv@126.com>
2022-03-19 15:54:14 +0800

    606. 根据二叉树创建字符串

---
+++ Sort/Leetcode/Problems/construct-string-from-binary-tree.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 606. 根据二叉树创建字符串
+    /// https://leetcode-cn.com/problems/construct-string-from-binary-tree/
+    /// </summary>
+    public class construct_string_from_binary_tree
+    {
+        private const string _left = "(";
+        private const string _right = ")";
+
+        /// <summary>
+        /// 递归
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public string Tree2str(TreeNode root)
+        {
+            var stringBuilder = new StringBuilder();
+            ToPreOrder(root, stringBuilder);
+            return stringBuilder.ToString();
+        }
+
+        private void ToPreOrder(TreeNode root, StringBuilder stringBuilder)
+        {
+            if (root == null)
+                return;
+            
+            //1. add root
+            stringBuilder.Append(root.val.ToString());
+
+            if (root.left == null && root.right == null)
+                return;
+
+
+            //2. only left
+            if (root.left != null && root.right == null)
+            {
+                //有子节点,添加括号
+                stringBuilder.Append(_left);
+                ToPreOrder(root.left, stringBuilder);
+                //有子节点,添加括号
+                stringBuilder.Append(_right);
+            }
+
+            //2. only right
+            else if (root.left != null && root.right == null)
+            {
+                //空括号左节点
+                stringBuilder.Append(_left);
+                stringBuilder.Append(_right);
+
+                ToPreOrder(root.left, stringBuilder);
+            }
+
+            //3. both exist
+            else
+            {
+                //有子节点,添加括号
+                stringBuilder.Append(_left);
+                //左节点
+                ToPreOrder(root.left, stringBuilder);
+                //添加括号
+                stringBuilder.Append(_right);
+
+                //有子节点,添加括号
+                stringBuilder.Append(_left);
+                //右节点
+                ToPreOrder(root.right, stringBuilder);
+                //添加括号
+                stringBuilder.Append(_right);
+            }
+        }
+
+        /// <summary>
+        /// 递归优化
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public string Tree2str_V2(TreeNode root)
+        {
+            if (root == null) return "";
+
+            if(root.left != null && root.right != null)
+                return new StringBuilder().Append(root.val.ToString())
+                    .Append(_left)
+                    .Append(Tree2str_V2(root.left))
+                    .Append(_right)
+                    .Append(_left)
+                    .Append(Tree2str_V2(root.right))
+                    .Append(_right)
+                    .ToString();
+
+            if (root.left == null && root.right != null)
+                return new StringBuilder().Append(root.val.ToString())
+                    .Append(_left)
+                    .Append(_right)
+                    .Append(_left)
+                    .Append(Tree2str_V2(root.right))
+                    .Append(_right)
+                    .ToString();
+            if (root.left != null && root.right == null)
+                return new StringBuilder().Append(root.val.ToString())
+                    .Append(_left)
+                    .Append(Tree2str_V2(root.left))
+                    .Append(_right)
+                    .ToString();
+
+            return root.val.ToString();
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/construct_string_from_binary_tree-test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class construct_string_from_binary_tree_test
+    {
+        construct_string_from_binary_tree target = new construct_string_from_binary_tree();
+        
+        static IEnumerable<TestCaseData> geTestCaseDatas
+        {
+            get
+            {
+                yield return new TestCaseData(new TreeNode(1,new TreeNode(2, new TreeNode(4)),new TreeNode(3))).Returns("1(2(4))(3)");
+                yield return new TestCaseData(new TreeNode(1, new TreeNode(2,null, new TreeNode(4)), new TreeNode(3))).Returns("1(2()(4))(3)");
+            }
+        }
+
+        [Test]
+        [TestCaseSource("geTestCaseDatas")]
+        public string test(TreeNode root)
+        {
+            return  target.Tree2str(root);
+        }
+
+        [Test]
+        [TestCaseSource("geTestCaseDatas")]
+        public string test_V2(TreeNode root)
+        {
+            return target.Tree2str_V2(root);
+        }
+    }
+}
