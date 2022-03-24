45caa120bb938f7010371ba175ddac0b9c0ff834
ManagerSi <smallvv@126.com>
2022-03-24 22:48:00 +0800

    661. 图片平滑器

---
+++ Sort/Leetcode/Problems/image-smoother.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    public class image_smoother
+    {
+        public int[][] ImageSmoother(int[][] img)
+        {
+            int[][] imgResult = new int[img.Length][];
+
+            for (int i = 0; i < img.Length; i++)
+            {
+                imgResult[i] = new int[img[i].Length];
+                for (int j = 0; j < img[i].Length; j++)
+                {
+                    imgResult[i][j] = GetAverageValue(img, i, j);
+                }
+            }
+
+            return imgResult;
+        }
+
+        private int GetAverageValue(int[][] img, int i, int j)
+        {
+            //self
+            var count = 1;
+            var summary = img[i][j];
+
+            var hasLeft = j - 1 >= 0;
+            var hasUp = i - 1 >= 0;
+            var hasRight = j + 1 < img[i].Length;
+            var hasDown = i + 1 < img.Length;
+
+
+            //左上
+            if (hasLeft && hasUp)
+            {
+                count++;
+                summary += img[i-1][j-1];
+            }
+            //上
+            if (hasUp)
+            {
+                count++;
+                summary += img[i-1][j];
+            }
+            //右上
+            if (hasUp && hasRight)
+            {
+                count++;
+                summary += img[i-1][j+1];
+            }
+            //左
+            if (hasLeft)
+            {
+                count++;
+                summary += img[i][j-1];
+            }
+            //右
+            if (hasRight)
+            {
+                count++;
+                summary += img[i][j+1];
+            }
+            //左下
+            if (hasLeft && hasDown)
+            {
+                count++;
+                summary += img[i+1][j-1];
+            }
+            //下
+            if (hasDown)
+            {
+                count++;
+                summary += img[i+1][j];
+            }
+            //右下
+            if (hasRight && hasDown)
+            {
+                count++;
+                summary += img[i+1][j+1];
+            }
+
+            return (summary / count);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/image-smoother-test.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    class image_smoother_test
+    {
+        image_smoother target = new image_smoother();
+
+        //public static IEnumerable<TestCaseData> GetTestCase()
+        //{
+        //    yield return new TestCaseData(new int[][]
+        //    {
+        //        new int[] {1, 1, 1},
+        //        new int[] {1, 0, 1},
+        //        new int[] {1, 1, 1}
+        //    }).Returns("0,0,0,0,0,0,0,0,0");
+        //}
+
+        [Test]
+       // [TestCaseSource("GetTestCase")]
+       public void test()
+       {
+           var request = new int[][]
+           {
+               new int[] {1, 1, 1},
+               new int[] {1, 0, 1},
+               new int[] {1, 1, 1}
+           };
+
+            var res = target.ImageSmoother(request);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < res.Length; i++)
+            {
+                for (int j = 0; j < res[i].Length; j++)
+                {
+                    sb.Append($"{res[i][j]},");
+                }
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+            Assert.AreEqual("0,0,0,0,0,0,0,0,0",sb.ToString());
+
+            /////////////////////////////////////////////////////////////////////////////////
+            
+            request = new int[][]
+            {
+                new int[] { 100, 200, 100 },
+                new int[] { 200, 50, 200 },
+                new int[] { 100, 200, 100 }
+            };
+
+            res = target.ImageSmoother(request);
+
+            sb.Clear();
+            for (int i = 0; i < res.Length; i++)
+            {
+                for (int j = 0; j < res[i].Length; j++)
+                {
+                    sb.Append($"{res[i][j]},");
+                }
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+            Assert.AreEqual("137,141,137,141,138,141,137,141,137", sb.ToString());
+        }
+    }
+}
