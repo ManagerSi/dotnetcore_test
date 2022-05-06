2acb739ca7708546baedaa9778adf4fb1e49c7ec
ManagerSi <smallvv@126.com>
2022-05-06 23:35:02 +0800

    933. 最近的请求次数

---
+++ Sort/Leetcode/Problems/number-of-recent-calls.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 933. 最近的请求次数
+    /// https://leetcode-cn.com/problems/number-of-recent-calls/
+    /// </summary>
+    public class number_of_recent_calls
+    {
+        private Queue<int> _q;
+        public number_of_recent_calls()
+        {
+            _q = new Queue<int>(1000);
+        }
+
+        /// <summary>
+        /// 并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。即只记录最近3000ms的请求
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public int Ping(int t)
+        {
+            _q.Enqueue(t);
+            while (_q.Peek() < t - 3000)
+            {
+                _q.Dequeue();
+            }
+            return _q.Count;
+        }
+
+    }
+}
---
+++ Sort/LeetcodeTests/number-of-recent-calls-test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    internal class number_of_recent_calls_test
+    {
+        public number_of_recent_calls target;
+
+        [SetUp]
+        public void setup()
+        {
+            target = new number_of_recent_calls();
+        }
+        [TestCase(new int[]{1,100,3001,3002}, ExpectedResult = "1,2,3,3")]
+        public string test(int[] args)
+        {
+            List<int> results = new List<int>();
+            //args always has value
+            foreach (var i in args)
+            {
+                results.Add(target.Ping(i));
+            }
+            return string.Join(',',results);
+        }
+    }
+}
