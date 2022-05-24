c50ff5ea739302da36f721f371ff2ec43646d551
ManagerSi <smallvv@126.com>
2022-05-24 22:30:09 +0800

    965. 单值二叉树

---
+++ Sort/Leetcode/Problems/univalued-binary-tree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 965. 单值二叉树
+    /// https://leetcode.cn/problems/univalued-binary-tree/
+    /// </summary>
+    public class univalued_binary_tree
+    {
+        /// <summary>
+        /// 深度优先遍历
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public bool IsUnivalTree(TreeNode root)
+        {
+            return CheckNodeValue(root, root.val);
+        }
+
+        /// <summary>
+        /// 深度优先遍历 dts
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        private bool CheckNodeValue(TreeNode root, int val)
+        {
+            if (root.val != val)
+                return false;
+
+            if (root.left != null)
+            {
+                if (!CheckNodeValue(root.left, val))
+                    return false;
+            }
+
+            if (root.right != null)
+                if (!CheckNodeValue(root.right, val))
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/univalued-binary-tree-test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Model;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    internal class univalued_binary_tree_test
+    {
+        private univalued_binary_tree target = new univalued_binary_tree();
+
+        public static IEnumerable<TestCaseData> Cases()
+        {
+            yield return new TestCaseData(new TreeNode(1, new TreeNode(1), new TreeNode(1)))
+                .Returns(true);
+
+            yield return new TestCaseData(new TreeNode(1, new TreeNode(2), null))
+                .Returns(false);
+
+            yield return new TestCaseData(new TreeNode(2, null, new TreeNode(2)))
+                .Returns(true);
+
+            yield return new TestCaseData(new TreeNode(1, new TreeNode(2), new TreeNode(1)))
+                .Returns(false);
+
+        }
+
+        [Test]
+        [TestCaseSource("Cases")]
+        public bool test(TreeNode p)
+        {
+            return target.IsUnivalTree(p);
+        }
+    }
+}
