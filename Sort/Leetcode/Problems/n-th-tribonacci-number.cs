eb2784c6dac023b121637573edff188600a68821
ManagerSi <smallvv@126.com>
2022-09-03 21:59:53 +0800

    1137. 第 N 个泰波那契数

---
+++ Sort/Leetcode/Problems/n-th-tribonacci-number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    public class n_th_tribonacci_number
+    {
+        /// <summary>
+        /// 递归超时
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int Tribonacci(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+            return Tribonacci(n - 1) + Tribonacci(n - 2) + Tribonacci(n - 3);
+        }
+
+
+        /// <summary>
+        /// 直接用递归的话会超时，给定的通项公式可以进一步推导 T(n+3) = T(n+2) + T(n+1) +T(n+0)， T(n+4) = T(n+3) + T(n+2) + T(n+1)， 两者相减 T(n+4) - T(n+3) = T(n+3) - T(n)， 所以T(n) = 2T(n-1) - T(n+4)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int Tribonacci_V1(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+            if (n == 3) return 2;
+            if (n == 4) return 4;
+            return 2 * Tribonacci_V1(n - 1) - Tribonacci_V1(n - 4);
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/n-th-tribonacci-number-test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    internal class n_th_tribonacci_number_test
+    {
+        private n_th_tribonacci_number target = new n_th_tribonacci_number();
+
+        [Test]
+        public void test()
+        {
+            int input = 1;
+            var r = target.Tribonacci(input);
+            Assert.AreEqual(input, r);
+
+            input = 2;
+            r = target.Tribonacci(input);
+            Assert.AreEqual(1, r);
+        }
+
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(4, ExpectedResult = 4)]
+        [TestCase(23, ExpectedResult = 410744)]
+        public int test2(int n)
+        {
+            return target.Tribonacci(n);
+        }
+
+
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(4, ExpectedResult = 4)]
+        [TestCase(23, ExpectedResult = 410744)]
+        public int testV1_1(int n)
+        {
+            return target.Tribonacci_V1(n);
+        }
+    }
+}
