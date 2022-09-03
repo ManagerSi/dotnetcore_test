1f07458c914089f261f83b4769d995d272bd745f
ManagerSi <smallvv@126.com>
2022-09-03 21:43:39 +0800

    509. 斐波那契数

---
+++ Sort/LeetcodeTests/fibonacci-number-test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    public class fibonacci_number_test
+    {
+        private fibonacci_number target = new fibonacci_number();
+
+        [Test]
+        public void test()
+        {
+            int input = 1;
+            var r = target.Fib(input);
+            Assert.AreEqual(input, r);
+
+            input = 2;
+            r = target.Fib(input);
+            Assert.AreEqual(1, r);
+        }
+
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(1,ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(4, ExpectedResult = 3)]
+        public int test2(int n)
+        {
+            return target.Fib(n);
+        }
+    }
+}
