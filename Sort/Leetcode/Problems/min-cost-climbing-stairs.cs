bf73110a9d5be5c3db904bb0ab378800ed7a3d96
ManagerSi <smallvv@126.com>
2022-09-04 16:24:56 +0800

    746. 使用最小花费爬楼梯

---
+++ Sort/Leetcode/Problems/min-cost-climbing-stairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 746. 使用最小花费爬楼梯
+    /// https://leetcode.cn/problems/min-cost-climbing-stairs/
+    /// </summary>
+    public class min_cost_climbing_stairs
+    {
+        /// <summary>
+        /// 动态规划
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            int n = cost.Length;
+            int[] dp = new int[n + 1];
+            dp[0] = 0; dp[1] = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                dp[i] = Math.Min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+            }
+            return dp[n];
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/min-cost-climbing-stairs-test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    internal class min_cost_climbing_stairs_test
+    {
+        private min_cost_climbing_stairs target = new min_cost_climbing_stairs();
+
+        [Test]
+        [TestCase(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1},ExpectedResult = 6)]
+        public int test(int[] cost)
+        {
+            return target.MinCostClimbingStairs(cost);
+        }
+    }
+}
