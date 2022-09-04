35c5db1ffbcc0213cc0dd65689c366794db91294
ManagerSi <smallvv@126.com>
2022-09-04 15:56:27 +0800

    70. 爬楼梯

---
+++ Sort/Leetcode/Problems/climbing-stairs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 70. 爬楼梯
+    /// https://leetcode.cn/problems/climbing-stairs
+    /// </summary>
+    public class climbing_stairs
+    {
+        /// <summary>
+        /// 动态规划-从头计算
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int climbStairs(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+
+            int a = 1, b = 2, res = 0;
+            for (int i = 3; i <= n; i++)
+            {
+                res = a + b;
+                a = b;
+                b = res;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/climbing-stairs-test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    internal class climbing_stairs_test
+    {
+        private climbing_stairs target = new climbing_stairs();
+       
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 2)]
+        [TestCase(4, ExpectedResult = 5)]
+        [TestCase(30, ExpectedResult = 1346269)]
+        public int test(int n)
+        {
+            return target.climbStairs(n);
+        }
+    }
+}
