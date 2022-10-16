c5b2879b183d43623317ae21af9ce858188e59fc
ManagerSi <smallvv@126.com>
2022-10-16 17:17:54 +0800

    886. 可能的二分法 https://leetcode.cn/problems/possible-bipartition/

---
+++ Sort/LeetcodeTests/possible-bipartition-test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Leetcode.Problems;
+using NUnit.Framework;
+
+namespace LeetcodeTests
+{
+    internal class possible_bipartition_test
+    {
+        private possible_bipartition target = new possible_bipartition();
+
+
+        [Test]
+        public void Test()
+        {
+            int n = 4;
+            int[][] dislikes = new int[][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 1, 3 },
+                new int[] { 2, 4 },
+            };
+            var result = target.PossibleBipartition(n, dislikes);
+            Assert.IsTrue(result);
+        }
+}
+
+}
