ea16145da7f586084d5f3df6f86ce7f4ff33875c
ManagerSi <smallvv@126.com>
2025-09-19 23:30:16 +0800

    添加算法题 merge_sorted_array

---
+++ Sort/Leetcode/Problems/merge-sorted-array.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Problems
+{
+    /// <summary>
+    /// 88. 合并两个有序数组
+    /// https://leetcode.cn/problems/merge-sorted-array/description/
+    /// </summary>
+    public class merge_sorted_array
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (n == 0) return;
+
+            if (m == 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    nums1[i] = nums2[i];
+                }
+                return;
+            }
+
+            int[] temp = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                temp[i] = nums1[i];
+            }
+            int min = m < n ? m : n;
+            int tempM = 0;
+            int tempN = 0;
+
+            for (int i = 0; i < m + n; i++)
+            {
+                if (temp[tempM] <= nums2[tempN])
+                {
+                    nums1[i] = temp[tempM];
+                    tempM++;
+                }
+                else
+                {
+                    nums1[i] = nums2[tempN];
+                    tempN++;
+                }
+                //Console.WriteLine(nums1[i]);
+                if (tempM == m)
+                {
+                    for (i++; i < m + n; i++)
+                    {
+                        nums1[i] = nums2[tempN];
+                        tempN++;
+                    }
+                    break;
+                }
+                if (tempN == n)
+                {
+                    for (i++; i < m + n; i++)
+                    {
+                        nums1[i] = temp[tempM];
+                        tempM++;
+                    }
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Sort/LeetcodeTests/merge_sorted_array-test.cs
@@ -0,0 +1,27 @@
+﻿using Leetcode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetcodeTests
+{
+    internal class merge_sorted_array_test
+    {
+        merge_sorted_array target = new merge_sorted_array();
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, ExpectedResult = "122356")]
+        [TestCase(new int[] { 1 }, 1, new int[] { }, 0, ExpectedResult = "1")]
+        [TestCase(new int[] { 1, 2 }, 2, new int[] {  }, 0, ExpectedResult = "12")]
+        [TestCase(new int[] { 1, 0 }, 1, new int[] { 1 }, 1, ExpectedResult = "11")]
+        [TestCase(new int[] { 0 }, 0, new int[] { 1 }, 1, ExpectedResult = "1")]
+        [TestCase(new int[] { 0, 0 }, 0, new int[] { 1, 2 }, 2, ExpectedResult = "12")]
+        public string test(int[] input, int m, int[] input2, int n)
+        {
+            target.Merge(input, m, input2, n);
+            return string.Join("", input);
+        }
+    }
+}
