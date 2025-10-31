a2a89ad614cad5b04af290e67bdef0607808a5b7
ManagerSi <smallvv@126.com>
2025-10-31 22:33:07 +0800

    修改dockerfile
    修改dockerfile，在dockerdemo.proj 同目录执行
    新增docker-compose.yml文件，在sln目录下执行

--- CoreTest/NetCore&WebAPI&Docker/src/V1/DockerDemo/DockerDemo/Program.cs
+++ CoreTest/NetCore&WebAPI&Docker/src/V1/DockerDemo/DockerDemo/Program.cs
@@ -1,7 +1,21 @@
-using NLog.Web;
+using Microsoft.AspNetCore.Diagnostics;
 using NLog.Targets;
+using NLog.Web;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    // 监听所有 IP 地址的 8080 端口（HTTP）
+    serverOptions.ListenAnyIP(8080);
+    // 监听本地回环地址的 5001 端口（HTTPS），并指定证书
+    //serverOptions.Listen(IPAddress.Loopback, 5001, listenOptions =>
+    //{
+    //    listenOptions.UseHttps("certificate.pfx", "password");
+    //});
+});
 
 //use nlog
 builder.WebHost.ConfigureAppConfiguration(webBuilder =>
@@ -23,14 +37,41 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+
+//捕获异常中间件
+
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        // using static System.Net.Mime.MediaTypeNames;
+        //context.Response.ContentType = Text.Plain;
+        //await context.Response.WriteAsync("api: An exception was thrown.");
+
+        Console.WriteLine("api: An exception was thrown.");
+
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        if (exceptionHandlerPathFeature?.Error is not null)
+        {
+            Console.WriteLine(exceptionHandlerPathFeature?.Error);
+        }
+
+        
+    });
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
